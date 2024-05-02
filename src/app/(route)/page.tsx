037817b423a2ddb9f6e5d207c321a23195c0d2fd
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-02 20:41:44 +0900

    🎨style: added Provider ul's gap property

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import useToast from "../_components/client/Toast/hooks/useToast/useToast";
+
 const Home = () => {
-  return <main className="w-full">DDuDu</main>;
+  const { createToast } = useToast();
+  return (
+    <main className="w-full">
+      DDuDu
+      <button
+        type="button"
+        onClick={() => createToast("fdsjkfbskdj")}
+      >
+        ddd
+      </button>
+    </main>
+  );
 };
 
 export default Home;
