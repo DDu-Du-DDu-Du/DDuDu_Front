f893f4e9698655b929e488fc87acc70aa76b3c2c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-03 00:11:30 +0900

    💎feat: added Toast options - type

---
+++ src/app/_components/client/Toast/components/ToastItem/hooks/useToastTypeColor/useToastTypeColor.ts
@@ -0,0 +1,30 @@
+import { ToastType } from "../../../../ToastProvider.type";
+
+interface UseToastTypeColor {
+  type: ToastType;
+}
+
+const useToastTypeColor = ({ type }: UseToastTypeColor) => {
+  /*
+    TODO
+    추후 정해지는 Color에 맞도록 변수 적용하기
+   */
+  switch (type) {
+    case "alert":
+      return "bg-example_orange_500";
+
+    case "safe":
+      return "bg-example_green_100";
+
+    case "warning":
+      return "bg-example_yellow_500";
+
+    case "danger":
+      return "bg-example_red_500";
+
+    default:
+      return "bg-example_orange_500";
+  }
+};
+
+export default useToastTypeColor;
