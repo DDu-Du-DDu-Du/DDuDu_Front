235013f34799bdd7dd024fc361a8f5ee9f93e10e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-02 22:45:22 +0900

    💎feat: added ToastItem's animation

--- src/app/_components/client/Toast/components/ToastItem/ToastItem.tsx
+++ src/app/_components/client/Toast/components/ToastItem/ToastItem.tsx
@@ -1,28 +1,46 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+import { AnimatePresence, motion } from "framer-motion";
 
 interface ToastProps {
   message: string;
+  deleteTime: number;
 }
 
-const ToastItem = ({ message }: ToastProps) => {
+const ToastItem = ({ message, deleteTime }: ToastProps) => {
+  const [isShow, setIsShow] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShow(false);
+    }, deleteTime);
+  }, [deleteTime]);
+
+  const initialStyle = {
+    translateX: "100%",
+    scale: 0.5,
+    opacity: 0,
+  };
+
   return (
-    <motion.li
-      initial={{
-        translateX: "100%",
-        scale: 0.5,
-        opacity: 0,
-      }}
-      animate={{
-        translateX: "0%",
-        scale: 1,
-        opacity: 1,
-      }}
-      className="w-[18rem] h-[6rem] bg-slate-400 rounded-radius10"
-    >
-      {message}
-    </motion.li>
+    <AnimatePresence>
+      {isShow && (
+        <motion.li
+          initial={initialStyle}
+          animate={{
+            translateX: "0%",
+            scale: 1,
+            opacity: 1,
+          }}
+          exit={initialStyle}
+          className="w-[18rem] h-[6rem] bg-slate-400 rounded-radius10"
+        >
+          {message}
+        </motion.li>
+      )}
+    </AnimatePresence>
   );
 };
 
