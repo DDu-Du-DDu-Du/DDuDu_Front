80bf5acc05e0d28f04e1dbb0371ef8f4862c76b4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-03 00:25:40 +0900

    🗑️remove: Toast Test code

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,20 +1,5 @@
-"use client";
-
-import useToast from "../_components/client/Toast/hooks/useToast/useToast";
-
 const Home = () => {
-  const { createToast } = useToast();
-  return (
-    <main className="w-full">
-      DDuDu
-      <button
-        type="button"
-        onClick={() => createToast("fdsjkfbskdj")}
-      >
-        ddd
-      </button>
-    </main>
-  );
+  return <main className="w-full h-full">DDuDu</main>;
 };
 
 export default Home;
