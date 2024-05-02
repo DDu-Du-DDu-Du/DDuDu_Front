8ff39f3528fe86e932561b9394cd63a7ebac457a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-02 20:46:46 +0900

    💎feat: added ToastItem's show animation

--- src/app/_components/client/Toast/components/ToastItem/ToastItem.tsx
+++ src/app/_components/client/Toast/components/ToastItem/ToastItem.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 interface ToastProps {
   message: string;
 }
 
 const ToastItem = ({ message }: ToastProps) => {
-  return <li className="w-[18rem] h-[6rem] bg-slate-400 rounded-radius10">{message}</li>;
+  return (
+    <motion.li
+      initial={{
+        translateX: "100%",
+        scale: 0.5,
+        opacity: 0,
+      }}
+      animate={{
+        translateX: "0%",
+        scale: 1,
+        opacity: 1,
+      }}
+      className="w-[18rem] h-[6rem] bg-slate-400 rounded-radius10"
+    >
+      {message}
+    </motion.li>
+  );
 };
 
 export default ToastItem;
