f3e37e01601fdcc36d884ff8b92504e7153c9c5e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-02 20:39:29 +0900

    💎feat: added createToast's deleteTime Options

--- src/app/_components/client/Toast/ToastProvider.type.ts
+++ src/app/_components/client/Toast/ToastProvider.type.ts
@@ -2,3 +2,9 @@ export interface ToastItemType {
   id: string;
   message: string;
 }
+
+export interface CreateToastOptionsType {
+  deleteTime?: number;
+}
+
+export type CreateToastFunc = (message: string, options?: CreateToastOptionsType) => void;
--- src/app/_components/client/Toast/hooks/useToastList/useToastList.ts
+++ src/app/_components/client/Toast/hooks/useToastList/useToastList.ts
@@ -2,6 +2,8 @@
 
 import { useCallback, useState } from "react";
 
+import { CreateToastFunc } from "../../ToastProvider.type";
+
 import { v4 as uuidV4 } from "uuid";
 
 const useToastList = () => {
@@ -11,15 +13,16 @@ const useToastList = () => {
     setToastList((prevToast) => prevToast.filter(({ id }) => id !== toastId));
   }, []);
 
-  const createToast = useCallback(
-    (message: string) => {
+  const createToast: CreateToastFunc = useCallback(
+    (message, options = {}) => {
+      const { deleteTime = 3000 } = options;
       const id = uuidV4();
 
       setToastList((prevToast) => [...prevToast, { id: id.toString(), message }]);
 
       setTimeout(() => {
         removeToast(id.toString());
-      }, 3000);
+      }, deleteTime);
     },
     [removeToast],
   );
