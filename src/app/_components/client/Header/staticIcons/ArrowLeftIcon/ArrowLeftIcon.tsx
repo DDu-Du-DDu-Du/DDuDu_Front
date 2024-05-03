10da78bcb0266b1e1d0e73c086fa69ce7734f2f3
Jaewoong Hwang <noa9925@gmail.com>
2024-05-03 17:25:25 +0900

    💎feat: leftButtonIcon 기본 아이콘 추가

---
+++ src/app/_components/client/Header/staticIcons/ArrowLeftIcon/ArrowLeftIcon.tsx
@@ -0,0 +1,19 @@
+const ArrowLeftIcon = () => {
+  return (
+    <svg
+      width="10"
+      height="16"
+      viewBox="0 0 10 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 1L2 9L12 17"
+        stroke="black"
+        stroke-width="1.5"
+      />
+    </svg>
+  );
+};
+
+export default ArrowLeftIcon;
