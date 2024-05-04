af6614c0085e2c542582bb2b7dc01d139249c460
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-04 16:52:20 +0900

    💎feat: BottomHeader forwardRef 적용 및 onMouseDown추가

--- src/app/_components/client/BottomSheet/components/BottomHeader/BottomHeader.tsx
+++ src/app/_components/client/BottomSheet/components/BottomHeader/BottomHeader.tsx
@@ -1,20 +1,31 @@
 "use client";
 
+import { MouseEvent, forwardRef } from "react";
+
 import tailwindConfig from "@/../tailwind.config";
 import { BarHandleIcon } from "@/app/_components/server";
 
 import resolveConfig from "tailwindcss/resolveConfig";
 
-const BottomHeader = () => {
+interface BottomHeaderProps {
+  onMouseDown: (event: MouseEvent<HTMLElement>) => void;
+}
+
+const BottomHeader = forwardRef<HTMLElement, BottomHeaderProps>(({ onMouseDown }, ref) => {
   const { theme } = resolveConfig(tailwindConfig);
   return (
-    <header className="w-full h-[3.2rem] flex items-center justify-center">
+    <header
+      className="w-full h-[3.2rem] flex items-center justify-center"
+      onMouseDown={onMouseDown}
+      ref={ref}
+    >
       <BarHandleIcon
         size={100}
         fill={theme.colors["example_gray_500"]}
       />
     </header>
   );
-};
+});
 
+BottomHeader.displayName = "BottomHeader";
 export default BottomHeader;
