22be841c570e31893c22c38576ab5c3726642f8c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-04 17:41:14 +0900

    💎feat: BottomSheet에 이벤트 적용

--- src/app/_components/client/BottomSheet/BottomSheet.tsx
+++ src/app/_components/client/BottomSheet/BottomSheet.tsx
@@ -1,32 +1,56 @@
 "use client";
 
 import { BottomHeader, BottomPortal } from "./components";
-import { useSheetAnimationState } from "./hooks";
+import { useChangeSheetHeight, useSheetAnimationState, useSheetDrag } from "./hooks";
 
 import { AnimatePresence, motion } from "framer-motion";
 
 interface BottomSheetProps {
   children: React.ReactNode;
   isShow: boolean;
+
+  defaultHeight?: string | number;
+  maxHeight?: string | number;
 }
 
-const BottomSheet = ({ isShow, children }: BottomSheetProps) => {
+const BottomSheet = ({
+  isShow,
+  children,
+  defaultHeight = "35dvh",
+  maxHeight = "80dvh",
+}: BottomSheetProps) => {
   const { isOpenSheet, activePortal, handleCloseSheet } = useSheetAnimationState({ isShow });
+
+  const { targetRef, handleDragStart, movementHeight, sheetState } = useSheetDrag();
+
+  const sheetHeight = useChangeSheetHeight({ sheetState, defaultHeight, maxHeight });
+
   return (
     <BottomPortal isShow={activePortal}>
       <AnimatePresence onExitComplete={handleCloseSheet}>
         {isOpenSheet && (
           <motion.article
             initial={{ y: "100%" }}
-            animate={{ y: `0%` }}
+            animate={{ y: `0%`, height: sheetHeight }}
             exit={{ y: "100%" }}
             transition={{
               duration: 0.3,
               ease: "easeInOut",
             }}
+            whileTap={{
+              height: movementHeight ? `${movementHeight}px` : sheetHeight,
+              transition: { duration: 0 },
+            }}
             className="w-full rounded-tl-[2.4rem] rounded-tr-[2.4rem] bg-slate-500 overflow-hidden"
+            style={{
+              maxHeight: maxHeight,
+              minHeight: "3.2rem",
+            }}
           >
-            <BottomHeader />
+            <BottomHeader
+              onMouseDown={handleDragStart}
+              ref={targetRef}
+            />
             {children}
           </motion.article>
         )}
