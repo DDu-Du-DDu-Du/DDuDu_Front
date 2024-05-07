717d51f8e8511a34b549e6a6febd1348850c7942
Jaewoong Hwang <noa9925@gmail.com>
2024-05-08 00:15:03 +0900

    🛠️refactor: useSegmentConvert 훅 로직 개선
    - useEffect, useState로 구성된 기존 로직을 useMemo로 통합
    - LeftIcon, LeftFunction 정적인 값 부여 (뒤로가기 버튼으로 고정)

--- src/app/_components/client/Header/Header.tsx
+++ src/app/_components/client/Header/Header.tsx
@@ -1,33 +1,33 @@
 "use client";
 
+import ArrowLeftIcon from "../../server/Icons/staticIcons/ArrowLeftIcon/ArrowLeftIcon";
 import HeaderButton from "./components/HeaderButton/HeaderButton";
 import HeaderLabel from "./components/HeaderLabel/HeaderLabel";
 import useSegmentConvert from "./hooks/useSegmentConvert/useSegmentConvert";
 
-import { useSelectedLayoutSegments } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
-  const segments = useSelectedLayoutSegments();
-  const { visible, headerLabel, leftButtonIcon, leftButtonFn, rightButtonIcon, rightButtonFn } =
-    useSegmentConvert({ segments });
-
-  if (!visible) {
-    return <></>;
-  }
+  const router = useRouter();
+  const { headerLabel, rightButtonIcon, rightButtonFn } = useSegmentConvert();
 
   return (
     <nav className="fixed top-0 flex h-[5.2rem] w-full max-w-[60rem] items-center justify-center">
       <HeaderButton
         buttonPosition="LEFT"
-        buttonFn={leftButtonFn}
-        buttonIcon={leftButtonIcon}
-      />
+        buttonFn={() => router.back()}
+      >
+        <ArrowLeftIcon />
+      </HeaderButton>
+
       <HeaderLabel label={headerLabel} />
+
       <HeaderButton
         buttonPosition="RIGHT"
         buttonFn={rightButtonFn}
-        buttonIcon={rightButtonIcon}
-      />
+      >
+        {rightButtonIcon}
+      </HeaderButton>
     </nav>
   );
 };
