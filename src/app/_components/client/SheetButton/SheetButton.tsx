19bb962007a50c583fa1e82b620c559cfce239b4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-07 23:26:12 +0900

    💎feat: SheetButton type props 추가 & animation

--- src/app/_components/client/SheetButton/SheetButton.tsx
+++ src/app/_components/client/SheetButton/SheetButton.tsx
@@ -2,25 +2,31 @@
 
 import ExampleIcon from "../../server/ExampleIcon/ExampleIcon";
 
+import { HTMLMotionProps, motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
 
-interface SheetButtonProps {
+interface SheetButtonProps extends HTMLMotionProps<"button"> {
+  buttonType?: "large" | "small";
   title: string;
   className?: string;
 }
 
-const SheetButton = ({ title, className }: SheetButtonProps) => {
+const SheetButton = ({ title, className, ...rest }: SheetButtonProps) => {
   return (
-    <button
+    <motion.button
       type="button"
       className={twMerge(
         "h-[8rem] flex-grow-[2] bg-example_gray_100 text-size13 flex flex-col items-center justify-center gap-[0.6rem] rounded-radius10",
         className,
       )}
+      whileTap={{
+        scale: 0.9,
+      }}
+      {...rest}
     >
       <ExampleIcon />
       {title}
-    </button>
+    </motion.button>
   );
 };
 
