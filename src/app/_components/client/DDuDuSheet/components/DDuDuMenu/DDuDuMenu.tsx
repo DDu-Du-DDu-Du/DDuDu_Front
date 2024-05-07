c5a812ab915ab840088b59ee73231a61612a74ae
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-07 23:49:49 +0900

    💎feat: DDuDuMenu 에 small 버튼 적용

--- src/app/_components/client/DDuDuSheet/components/DDuDuMenu/DDuDuMenu.tsx
+++ src/app/_components/client/DDuDuSheet/components/DDuDuMenu/DDuDuMenu.tsx
@@ -2,7 +2,7 @@ import { DDuDuMainMenu, DDuDuSubMenu } from "./components";
 
 export const DDuDuMenu = () => {
   return (
-    <div className="w-ful flex flex-col items-center p-[2rem]">
+    <div className="w-ful flex flex-col items-center p-[2rem] gap-[2.5rem]">
       <DDuDuMainMenu />
       <DDuDuSubMenu />
     </div>
--- src/app/_components/client/DDuDuSheet/components/DDuDuMenu/components/DDuDuSubMenu/DDuDuSubMenu.tsx
+++ src/app/_components/client/DDuDuSheet/components/DDuDuMenu/components/DDuDuSubMenu/DDuDuSubMenu.tsx
@@ -1,5 +1,23 @@
+import SheetButton from "@/app/_components/client/SheetButton/SheetButton";
+
 const DDuDuSubMenu = () => {
-  return <div>DDuDuSubMenu</div>;
+  return (
+    <div className="w-full max-w-[50rem] flex flex-col gap-[1.6rem]">
+      <SheetButton
+        title="알림 설정하기"
+        buttonType="small"
+        rightPlace={<p>5 일전</p>}
+      />
+      <SheetButton
+        title="다른날 반복하기"
+        buttonType="small"
+      />
+      <SheetButton
+        title="미루기"
+        buttonType="small"
+      />
+    </div>
+  );
 };
 
 export default DDuDuSubMenu;
