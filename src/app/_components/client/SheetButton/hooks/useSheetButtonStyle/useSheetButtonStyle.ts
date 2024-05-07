bddd0294593a6d6cdb1da64a1e4628c552c2f8f6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-07 23:48:42 +0900

    💎feat: useSheetButtonStyle hook 생성

---
+++ src/app/_components/client/SheetButton/hooks/useSheetButtonStyle/useSheetButtonStyle.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { twJoin } from "tailwind-merge";
+
+interface UseSheetButtonStyleProps {
+  buttonType: "large" | "small";
+}
+
+const useSheetButtonStyle = ({ buttonType }: UseSheetButtonStyleProps) => {
+  return twJoin(
+    "text-size13 flex items-center",
+
+    buttonType === "large" &&
+      "h-[8rem] flex-grow-[2] bg-example_gray_100 flex-col justify-center gap-[0.6rem] rounded-radius10",
+
+    buttonType === "small" && "w-full gap-[1.6rem]",
+  );
+};
+
+export default useSheetButtonStyle;
