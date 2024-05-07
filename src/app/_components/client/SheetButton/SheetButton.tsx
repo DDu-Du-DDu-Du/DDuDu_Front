1ad467760e0ee159c6924dc63c49586112220747
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-07 23:40:23 +0900

    💎feat: SheetButton Small type 구현 & 스타일링

--- src/app/_components/client/SheetButton/SheetButton.tsx
+++ src/app/_components/client/SheetButton/SheetButton.tsx
@@ -3,31 +3,43 @@
 import ExampleIcon from "../../server/ExampleIcon/ExampleIcon";
 
 import { HTMLMotionProps, motion } from "framer-motion";
-import { twMerge } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 
 interface SheetButtonProps extends HTMLMotionProps<"button"> {
   buttonType?: "large" | "small";
   title: string;
+
+  rightPlace?: React.ReactNode;
   className?: string;
 }
 
-const SheetButton = ({ title, className, ...rest }: SheetButtonProps) => {
+const SheetButton = ({
+  buttonType = "small",
+  title,
+  rightPlace,
+  className,
+  ...rest
+}: SheetButtonProps) => {
   return (
     <motion.button
       type="button"
-      className={twMerge(
-        "h-[8rem] flex-grow-[2] bg-example_gray_100 text-size13 flex flex-col items-center justify-center gap-[0.6rem] rounded-radius10",
-        className,
-      )}
+      className={twMerge("w-full flex items-center gap-[1.6rem] text-size13", className)}
       whileTap={{
         scale: 0.9,
       }}
       {...rest}
     >
       <ExampleIcon />
-      {title}
+
+      <p className={twJoin(buttonType === "small" && "flex-grow text-start")}>{title}</p>
+
+      {buttonType === "small" && rightPlace && rightPlace}
     </motion.button>
   );
 };
 
 export default SheetButton;
+
+// "w-full flex items-center gap-[1.6rem] text-size13"
+
+//  "h-[8rem] flex-grow-[2] bg-example_gray_100 text-size13 flex flex-col items-center justify-center gap-[0.6rem] rounded-radius10",
