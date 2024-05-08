f92010aefaa28d92ff0b3e1faa1111d5b1fdbb1a
seunghyun <tmdgus7820@naver.com>
2024-05-08 13:43:24 +0900

    📝docs: 첨부 파일 확장자 및 avatar size에 대한 상수 파일 추가

---
+++ src/app/_components/client/Avatar/Avatar.constant.ts
@@ -0,0 +1,19 @@
+export const IMAGE_TYPES: { [key: string]: boolean } = {
+  "image/gif": true,
+  "image/jpeg": true,
+  "image/png": true,
+  "image/jpg": true,
+  "image/svg+xml": true,
+};
+
+export const AVATAR_SIZE = {
+  small: "size-[4rem]",
+  medium: "size-[8rem]",
+  large: "size-[10rem]",
+};
+
+export const AVATAR_ICON = {
+  small: "size-[1.1rem]",
+  medium: "size-[2.2rem]",
+  large: "size-[2.8rem]",
+};
