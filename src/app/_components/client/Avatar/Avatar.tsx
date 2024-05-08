c99fdb1dbf665da91a796fc81043d80d7d5bcdaa
seunghyun <tmdgus7820@naver.com>
2024-05-08 13:52:21 +0900

    💎feat: Avatar 컴포넌트 추가 - type을 받아 view, edit 역할을 하는 컴포넌트 - view 아바타를 볼 수 있고 아바타를 클릭 시 해당 유저 페이지로 이동합니다. - edit 아바타를 볼 수 있고 아바타를 클릭 시 아바타 이미지를 변경할 수 있습니다.

---
+++ src/app/_components/client/Avatar/Avatar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import profileImage from "../../../../../public/assets/userProfile.svg";
+import { AVATAR_ICON, AVATAR_SIZE } from "./Avatar.constant";
+import { useChangeImage, useClickAvatar } from "./hooks";
+
+import Image from "next/image";
+
+export interface AvatarProps {
+  type?: "view" | "edit";
+  size?: "small" | "medium" | "large";
+  avatarImage: string | null;
+  userId?: string;
+  onChangeAvatar?: (file: File | null) => void;
+}
+
+const Avatar = ({
+  type = "view",
+  size = "small",
+  avatarImage = null,
+  userId,
+  onChangeAvatar,
+}: AvatarProps) => {
+  const { preview, inputRef, handleChangeImage } = useChangeImage({ avatarImage, onChangeAvatar });
+  const { handleClickAvatar } = useClickAvatar({ type, inputRef, userId, onChangeAvatar });
+  const avatarImageSize = AVATAR_SIZE[size];
+  const avatarIconSize = AVATAR_ICON[size];
+
+  return (
+    <div
+      className={`relative ${avatarImageSize} ${(userId || type === "edit") && "cursor-pointer"}`}
+      onClick={handleClickAvatar}
+    >
+      <div className="size-[100%] bg-gray-300 rounded-circle  overflow-hidden">
+        <Image
+          src={preview || profileImage}
+          alt="프로필 이미지 사진"
+          sizes="100%"
+          fill
+          priority
+        />
+      </div>
+
+      {type === "edit" && (
+        <>
+          <span
+            className={`absolute bottom-0 right-0 ${avatarIconSize} bg-[#F1F1F1] rounded-full`}
+          ></span>
+          <input
+            className="hidden"
+            ref={inputRef}
+            type="file"
+            name="profile"
+            id="profile"
+            onChange={handleChangeImage}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Avatar;
