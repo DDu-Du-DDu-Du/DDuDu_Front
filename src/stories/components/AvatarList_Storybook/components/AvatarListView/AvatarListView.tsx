71ee07cecf0768274eb76b7b1e80b3e32fa1d6e3
seunghyun <tmdgus7820@naver.com>
2024-05-08 19:43:10 +0900

    💎feat: AvatarList Storybook 추가

---
+++ src/stories/components/AvatarList_Storybook/components/AvatarListView/AvatarListView.tsx
@@ -0,0 +1,7 @@
+import AvatarList, { AvatarListProps } from "@/app/_components/client/AvatarList/AvatarList";
+
+const AvatarListView = (props: AvatarListProps) => {
+  return <AvatarList {...props} />;
+};
+
+export default AvatarListView;
---
+++ src/stories/components/AvatarList_Storybook/components/index.ts
@@ -0,0 +1,1 @@
+export { default as AvatarListView } from "./AvatarListView/AvatarListView";
