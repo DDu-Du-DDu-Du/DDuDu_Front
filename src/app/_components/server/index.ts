192106efc2681936445ad35f30015f37405293d0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 03:06:12 +0900

    💎feat: Timeline Component 구현

--- src/app/_components/server/index.ts
+++ src/app/_components/server/index.ts
@@ -2,3 +2,5 @@ export * from "./icons";
 
 export { default as ExampleIcon } from "./ExampleIcon/ExampleIcon";
 export { default as GoalTodoListItem } from "./GoalTodoListItem/GoalTodoListItem";
+
+export { default as Timeline } from "./Timeline/Timeline";
