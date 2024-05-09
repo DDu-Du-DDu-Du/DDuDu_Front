4256be1ace6eb42d893b4d08911ea1d4d8bd2e93
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 03:05:37 +0900

    💎feat: LineBox Component 구현

---
+++ src/app/_components/server/Timeline/components/LineBox/LineBox.tsx
@@ -0,0 +1,9 @@
+const LineBox = () => {
+  return (
+    <div className=" absolute left-[5rem] flex h-full w-[5rem] justify-center">
+      <hr className="h-full w-[0.3rem] bg-example_gray_300" />
+    </div>
+  );
+};
+
+export default LineBox;
---
+++ src/app/_components/server/Timeline/components/index.ts
@@ -0,0 +1,2 @@
+export { default as TimelineItem } from "./TimelineItem/TimelineItem";
+export { default as LineBox } from "./LineBox/LineBox";
