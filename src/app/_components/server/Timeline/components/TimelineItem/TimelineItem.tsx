4982464f8eef9fe38771cc259d20cd9c381c2677
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 03:04:51 +0900

    💎feat: TimelineItem Component 구현

---
+++ src/app/_components/server/Timeline/components/TimelineItem/TimelineItem.tsx
@@ -0,0 +1,37 @@
+import { TimeItem, TimeStamp } from "./components";
+
+interface TimeItemType {
+  todo: string;
+  startAt: string;
+  endAt: string;
+}
+
+interface TimelineItemProps {
+  baseTime: string;
+  TodoList: TimeItemType[];
+}
+
+const TimelineItem = ({ baseTime, TodoList }: TimelineItemProps) => {
+  return (
+    <li className="flex w-full">
+      <TimeStamp>{baseTime}</TimeStamp>
+
+      <div className="w-[5rem]" />
+
+      <ul className="flex flex-1 flex-col gap-[1rem] pr-[1rem]">
+        {TodoList.map(({ todo, startAt, endAt }, index) => (
+          <TimeItem
+            key={todo}
+            todo={todo}
+            startTime={startAt}
+            endTime={endAt}
+            isLastItem={index === TodoList.length - 1}
+            themeColor="#000"
+          />
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+export default TimelineItem;
---
+++ src/app/_components/server/Timeline/components/TimelineItem/components/index.ts
@@ -0,0 +1,2 @@
+export { default as TimeStamp } from "./TimeStamp/TimeStamp";
+export { default as TimeItem } from "./TimeItem/TimeItem";
