fd210e6b5a826319a2ee2e8c0146c013220ebb5c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-09 15:31:21 +0900

    💎feat: useClickAway hook 구현

--- src/app/_hooks/useClickAway/useClickAway.ts
+++ src/app/_hooks/useClickAway/useClickAway.ts
@@ -1,7 +1,52 @@
 "use client";
 
-const useClickAway = () => {
-  return "";
+import { useEffect, useRef } from "react";
+
+import { CallbackType } from "./useClickAway.type";
+
+const EVENT_LIST: ["mousedown", "touchstart"] = ["mousedown", "touchstart"];
+
+const useClickAway = <T>(callback: CallbackType) => {
+  const ref = useRef<T | null>(null);
+  const memoCallback = useRef(callback);
+
+  useEffect(() => {
+    memoCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const { current } = ref;
+
+    if (!current || !(current instanceof HTMLElement)) {
+      return;
+    }
+
+    const eventHandler = (event: MouseEvent | TouchEvent) => {
+      const { target } = event;
+
+      if (!target || !(target instanceof HTMLElement)) {
+        return;
+      }
+
+      if (current.contains(target)) {
+        return;
+      }
+
+      memoCallback.current(event);
+    };
+
+    EVENT_LIST.forEach((eventName) => {
+      window.addEventListener(eventName, eventHandler);
+    });
+
+    return () => {
+      EVENT_LIST.forEach((eventName) => {
+        window.removeEventListener(eventName, eventHandler);
+      });
+    };
+  }, []);
+
+  return ref;
 };
 
 export default useClickAway;
