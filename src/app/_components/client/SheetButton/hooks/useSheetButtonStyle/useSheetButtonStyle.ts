d30407eb43df8fa0e9d845a176d9eb277e6e85ee
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 17:39:57 +0900

    🛠️refactor: type 메모이제이션

--- src/app/_components/client/SheetButton/hooks/useSheetButtonStyle/useSheetButtonStyle.ts
+++ src/app/_components/client/SheetButton/hooks/useSheetButtonStyle/useSheetButtonStyle.ts
@@ -1,5 +1,7 @@
 "use client";
 
+import { useMemo } from "react";
+
 import { twJoin } from "tailwind-merge";
 
 interface UseSheetButtonStyleProps {
@@ -7,14 +9,20 @@ interface UseSheetButtonStyleProps {
 }
 
 const useSheetButtonStyle = ({ buttonType }: UseSheetButtonStyleProps) => {
-  return twJoin(
-    "text-size13 flex items-center",
+  const style = useMemo(
+    () =>
+      twJoin(
+        "text-size13 flex items-center",
 
-    buttonType === "main" &&
-      "h-[8rem] flex-grow-[2] bg-example_gray_100 flex-col justify-center gap-[0.6rem] rounded-radius10",
+        buttonType === "main" &&
+          "h-[8rem] flex-grow-[2] bg-example_gray_100 flex-col justify-center gap-[0.6rem] rounded-radius10",
 
-    buttonType === "sub" && "w-full h-[3.2rem] gap-[1.6rem]",
+        buttonType === "sub" && "w-full h-[3.2rem] gap-[1.6rem]",
+      ),
+    [buttonType],
   );
+
+  return style;
 };
 
 export default useSheetButtonStyle;
