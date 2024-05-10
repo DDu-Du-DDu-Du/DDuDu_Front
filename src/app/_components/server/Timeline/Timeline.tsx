23492ba404fcd11393973d37ab739e351def7727
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 16:52:37 +0900

    💎feat: Timeline 뒷 배경 element 추가

--- src/app/_components/server/Timeline/Timeline.tsx
+++ src/app/_components/server/Timeline/Timeline.tsx
@@ -9,18 +9,20 @@ interface TimelineProps {
 
 const Timeline = ({ timeline }: TimelineProps) => {
   return (
-    <ul className="relative flex w-full min-w-[20rem] flex-col gap-[4rem]">
-      <LineBox />
+    <article className="w-full py-[1rem]">
+      <ul className="relative flex w-full min-w-[20rem] flex-col gap-[4rem]">
+        <LineBox />
 
-      {timeline &&
-        timeline.map(({ time, ddudus }, index) => (
-          <TimelineItem
-            key={`${time}${index}`}
-            baseTime={time}
-            dduduList={ddudus}
-          />
-        ))}
-    </ul>
+        {timeline &&
+          timeline.map(({ time, ddudus }, index) => (
+            <TimelineItem
+              key={`${time}${index}`}
+              baseTime={time}
+              dduduList={ddudus}
+            />
+          ))}
+      </ul>
+    </article>
   );
 };
 
