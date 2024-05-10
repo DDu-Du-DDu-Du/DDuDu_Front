918f33f1da84f560f80e0cbe98e70db32e82b456
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 16:22:33 +0900

    💎feat: 테스트 코드

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,12 +1,5 @@
-import DDuDuSheet from "../_components/client/DDuDuSheet/DDuDuSheet";
-
 const Home = () => {
-  return (
-    <main className="h-full w-full">
-      DDuDu
-      <DDuDuSheet />
-    </main>
-  );
+  return <main className="h-full w-full">DDuDu</main>;
 };
 
 export default Home;
