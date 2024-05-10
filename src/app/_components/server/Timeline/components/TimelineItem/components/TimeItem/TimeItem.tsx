c19d6e1eace00f2eedf74cceeeac1bb8c6d79e42
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 16:22:06 +0900

    🛠️refactor: Util 함수 적용 및 스타일 조정

--- src/app/_components/server/Timeline/components/TimelineItem/components/TimeItem/TimeItem.tsx
+++ src/app/_components/server/Timeline/components/TimelineItem/components/TimeItem/TimeItem.tsx
@@ -3,6 +3,8 @@
 import tailwindConfig from "@/../tailwind.config";
 import { TimelineDDuDuItemType } from "@/app/_types/response";
 
+import { hexConvertForRGBA } from "../../../../utils";
+
 import { motion } from "framer-motion";
 import resolveConfig from "tailwindcss/resolveConfig";
 
@@ -18,9 +20,17 @@ const TimeItem = ({ ddudu, isLastItem }: TimeItemProps) => {
   return (
     <li className="relative w-full">
       <div className="absolute right-[100%] top-0 flex h-full w-[5rem] items-center justify-center">
-        {/* 아이콘 */}
+        {/* middle icon */}
         <div className="absolute top-0 z-timeline_icon flex items-center justify-center h-[5.7rem]">
-          <div className="flex h-[2rem] w-[2rem] items-center justify-center rounded-circle bg-example_gray_900">
+          <div
+            className="flex h-[2.2rem] w-[2.2rem] items-center justify-center rounded-circle"
+            style={{
+              backgroundColor:
+                status === "COMPLETED"
+                  ? theme.colors["example_gray_900"]
+                  : theme.colors["example_gray_300"],
+            }}
+          >
             <div className="h-[1.6rem] w-[1.6rem] rounded-circle bg-white flex items-center justify-center">
               {/*              
                 TODO
@@ -34,6 +44,7 @@ const TimeItem = ({ ddudu, isLastItem }: TimeItemProps) => {
           </div>
         </div>
 
+        {/* middle timeline */}
         {!isLastItem && (
           <hr
             className="absolute top-[3rem] z-timeline_line h-full w-[0.3rem]"
@@ -45,20 +56,38 @@ const TimeItem = ({ ddudu, isLastItem }: TimeItemProps) => {
             style={{
               backgroundColor:
                 status === "COMPLETED"
-                  ? theme.colors["example_gray_100"]
-                  : theme.colors["example_gray_900"],
+                  ? theme.colors["example_gray_900"]
+                  : theme.colors["example_gray_300"],
             }}
           />
         )}
       </div>
 
-      {/* DDuDuItem */}
+      {/* DDuDu */}
       <motion.button
         whileTap={{ scale: 0.95 }}
-        className="w-full min-h-[5.7rem] flex flex-col rounded-radius15 px-[1.6rem] py-[1.2rem] shadow-example_shadow"
+        className="w-full min-h-[5.7rem] flex flex-col rounded-radius15 px-[1.6rem] py-[1.2rem]"
+        style={{
+          backgroundColor: hexConvertForRGBA({
+            /*              
+              TODO
+
+              추후 상단에 생성될 전체 목표 목록 스토어에서 일치하는 컬러 매칭하기 
+            */
+            hex: theme.colors["example_gray_900"],
+            alpha: 0.1,
+          }),
+        }}
         type="button"
+        onClick={() => {
+          /* 
+            TODO
+
+            추후 해당 DDuDu의 완료처리 로직
+          */
+        }}
       >
-        <p className="text-size14">{name}</p>
+        <p className="text-size14 text-start">{name}</p>
         <p className="text-size11 text-example_gray_900">{`${beginAt} - ${endAt}`}</p>
       </motion.button>
     </li>
