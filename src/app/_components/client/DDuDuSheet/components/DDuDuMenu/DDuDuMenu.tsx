1efd335d6eea7522657ecdd104c3ce950ce93126
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 17:41:26 +0900

    🛠️refactor: Type 및 변경 누락 사항

--- src/app/_components/client/DDuDuSheet/components/DDuDuMenu/DDuDuMenu.tsx
+++ src/app/_components/client/DDuDuSheet/components/DDuDuMenu/DDuDuMenu.tsx
@@ -2,7 +2,7 @@ import { DDuDuMainMenu, DDuDuSubMenu } from "./components";
 
 export const DDuDuMenu = () => {
   return (
-    <div className="w-ful flex flex-col items-center p-[2rem] gap-[2.5rem]">
+    <div className="w-ful flex flex-col items-center gap-[2.5rem] p-[2rem]">
       <DDuDuMainMenu />
       <DDuDuSubMenu />
     </div>
