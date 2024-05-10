902fec4980c301fe8c3a2fe50ff7766f76224741
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 14:53:14 +0900

    🛠️refactor: TimelineItem 조정

--- src/app/_components/server/Timeline/components/TimelineItem/components/TimeItem/TimeItem.tsx
+++ src/app/_components/server/Timeline/components/TimelineItem/components/TimeItem/TimeItem.tsx
@@ -2,14 +2,11 @@
 
 import tailwindConfig from "@/../tailwind.config";
 
-import { TIMELINE_ITEM_HEIGHT } from "../../../../Timeline.constants";
-
 import { motion } from "framer-motion";
-import { twJoin } from "tailwind-merge";
 import resolveConfig from "tailwindcss/resolveConfig";
 
 interface TimeItemProps {
-  todo: string;
+  name: string;
   startTime: string;
   endTime: string;
   isLastItem?: boolean;
@@ -18,7 +15,7 @@ interface TimeItemProps {
 }
 
 const TimeItem = ({
-  todo,
+  name,
   startTime,
   endTime,
   isLastItem = false,
@@ -31,24 +28,16 @@ const TimeItem = ({
     <li className="relative w-full">
       <div className="absolute right-[100%] top-0 flex h-full w-[5rem] items-center justify-center">
         {/* 아이콘 */}
-        <div
-          className={twJoin(
-            "absolute top-0 z-timeline_icon flex items-center justify-center",
-            TIMELINE_ITEM_HEIGHT,
-          )}
-        >
+        <div className="absolute top-0 z-timeline_icon flex items-center justify-center h-[5.7rem]">
           <div className="flex h-[2rem] w-[2rem] items-center justify-center rounded-circle bg-example_gray_900">
-            <div
-              className="h-[1.6rem] w-[1.6rem] rounded-circle bg-white"
-              /*
-              TODO
+            <div className="h-[1.6rem] w-[1.6rem] rounded-circle bg-white flex items-center justify-center">
+              {/*              
+                TODO
 
-              추후 상단에 생성될 전체 목표 목록 스토어에서 일치하는 컬러 매칭하기
-            */
-              style={{
-                backgroundColor: isCompleted ? themeColor : theme.colors["background"],
-              }}
-            />
+                추후 상단에 생성될 전체 목표 목록 스토어에서 일치하는 컬러 매칭하기 
+              */}
+              {true && <div className="h-[1.2rem] w-[1.2rem] bg-example_gray_900 rounded-circle" />}
+            </div>
           </div>
         </div>
 
@@ -70,10 +59,10 @@ const TimeItem = ({
       {/* DDuDuItem */}
       <motion.button
         whileTap={{ scale: 0.95 }}
-        className="w-full flex flex-col rounded-radius15 px-[1.6rem] py-[1.2rem] shadow-example_shadow"
+        className="w-full min-h-[5.7rem] flex flex-col rounded-radius15 px-[1.6rem] py-[1.2rem] shadow-example_shadow"
         type="button"
       >
-        <p className="text-size14">{todo}</p>
+        <p className="text-size14">{name}</p>
         <p className="text-size11 text-example_gray_900">{`${startTime} - ${endTime}`}</p>
       </motion.button>
     </li>
