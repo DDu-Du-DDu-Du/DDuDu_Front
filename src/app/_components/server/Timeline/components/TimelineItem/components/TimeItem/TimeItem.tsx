d040259dc741d2a2da0cc8b432e7c6647e67ce24
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 13:45:08 +0900

    🎨style: TimeItem 스타일 조정 & animation

--- src/app/_components/server/Timeline/components/TimelineItem/components/TimeItem/TimeItem.tsx
+++ src/app/_components/server/Timeline/components/TimelineItem/components/TimeItem/TimeItem.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import tailwindConfig from "@/../tailwind.config";
 
 import { TIMELINE_ITEM_HEIGHT } from "../../../../Timeline.constants";
 
+import { motion } from "framer-motion";
 import { twJoin } from "tailwind-merge";
 import resolveConfig from "tailwindcss/resolveConfig";
 
@@ -23,8 +26,9 @@ const TimeItem = ({
   themeColor,
 }: TimeItemProps) => {
   const { theme } = resolveConfig(tailwindConfig);
+
   return (
-    <li className="relative flex w-full flex-col rounded-radius15 px-[1.6rem] py-[1.2rem] shadow-example_shadow">
+    <li className="relative w-full">
       <div className="absolute right-[100%] top-0 flex h-full w-[5rem] items-center justify-center">
         {/* 아이콘 */}
         <div
@@ -36,6 +40,11 @@ const TimeItem = ({
           <div className="flex h-[2rem] w-[2rem] items-center justify-center rounded-circle bg-example_gray_900">
             <div
               className="h-[1.6rem] w-[1.6rem] rounded-circle bg-white"
+              /*
+              TODO
+
+              추후 상단에 생성될 전체 목표 목록 스토어에서 일치하는 컬러 매칭하기
+            */
               style={{
                 backgroundColor: isCompleted ? themeColor : theme.colors["background"],
               }}
@@ -43,15 +52,30 @@ const TimeItem = ({
           </div>
         </div>
 
-        {/* 구분선 */}
         {!isLastItem && (
-          <hr className="absolute top-[3rem] z-timeline_line h-full w-[0.3rem] bg-example_gray_900" />
+          <hr
+            className="absolute top-[3rem] z-timeline_line h-full w-[0.3rem]"
+            /*
+              TODO
+
+              추후 상단에 생성될 전체 목표 목록 스토어에서 일치하는 컬러 매칭하기
+            */
+            style={{
+              backgroundColor: isCompleted ? themeColor : theme.colors["example_gray_900"],
+            }}
+          />
         )}
       </div>
 
       {/* DDuDuItem */}
-      <p className="text-size14">{todo}</p>
-      <p className="text-size11 text-example_gray_900">{`${startTime} - ${endTime}`}</p>
+      <motion.button
+        whileTap={{ scale: 0.95 }}
+        className="w-full flex flex-col rounded-radius15 px-[1.6rem] py-[1.2rem] shadow-example_shadow"
+        type="button"
+      >
+        <p className="text-size14">{todo}</p>
+        <p className="text-size11 text-example_gray_900">{`${startTime} - ${endTime}`}</p>
+      </motion.button>
     </li>
   );
 };
