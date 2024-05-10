c8feb6ab2e335cd1127f80376705be5808c71e18
seunghyun <tmdgus7820@naver.com>
2024-05-11 03:47:36 +0900

    🛠️refactor: useChangeImage, useClickAvatar hook 최적화 - useCallback을 통한 성능 최적화

--- src/app/_components/client/Avatar/hooks/useChangeImage/useChangeImage.tsx
+++ src/app/_components/client/Avatar/hooks/useChangeImage/useChangeImage.tsx
@@ -1,43 +1,46 @@
-import { ChangeEventHandler, useRef, useState } from "react";
+import { ChangeEventHandler, useCallback, useRef, useState } from "react";
 
 import { IMAGE_TYPES } from "../../Avatar.constant";
 
 interface useChangeImageProps {
-  avatarImage: string | null;
+  userImage: string | null;
   onChangeAvatar?: (file: File | null) => void;
 }
 
-const useChangeImage = ({ avatarImage, onChangeAvatar }: useChangeImageProps) => {
-  const [preview, setPreview] = useState(avatarImage);
+const useChangeImage = ({ userImage, onChangeAvatar }: useChangeImageProps) => {
+  const [preview, setPreview] = useState(userImage);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleChangeImage: ChangeEventHandler<HTMLInputElement> = (event) => {
-    if (!event.target.files || !onChangeAvatar) {
-      return;
-    }
-
-    const file = event.target.files[0];
-    const fileType = file.type;
-
-    if (!IMAGE_TYPES[fileType]) {
-      alert("파일 형식이 올바르지 않습니다. 이미지 파일을 업로드해 주세요.");
-      event.target.value = "";
-      return;
-    }
-
-    const fileReader = new FileReader();
-
-    fileReader.onloadend = () => {
-      if (typeof fileReader.result !== "string") {
+  const handleChangeImage: ChangeEventHandler<HTMLInputElement> = useCallback(
+    (event) => {
+      if (!event.target.files || !onChangeAvatar) {
         return;
       }
 
-      setPreview(fileReader.result);
-    };
+      const file = event.target.files[0];
+      const fileType = file.type;
 
-    fileReader.readAsDataURL(file);
-    onChangeAvatar(file);
-  };
+      if (!IMAGE_TYPES[fileType]) {
+        alert("파일 형식이 올바르지 않습니다. 이미지 파일을 업로드해 주세요.");
+        event.target.value = "";
+        return;
+      }
+
+      const fileReader = new FileReader();
+
+      fileReader.onloadend = () => {
+        if (typeof fileReader.result !== "string") {
+          return;
+        }
+
+        setPreview(fileReader.result);
+      };
+
+      fileReader.readAsDataURL(file);
+      onChangeAvatar(file);
+    },
+    [onChangeAvatar, setPreview],
+  );
 
   return {
     preview,
