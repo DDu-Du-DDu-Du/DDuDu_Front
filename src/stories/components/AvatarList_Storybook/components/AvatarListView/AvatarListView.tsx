c834c91d206ba76efff28f0b77bc3f95e9382aa8
seunghyun <tmdgus7820@naver.com>
2024-05-11 02:59:18 +0900

    🗑️remove: AvatarList 컴포넌트 제거 - Avatar 컴포넌트 내부 컴포넌트로 통합했습니다.

--- src/stories/components/AvatarList_Storybook/components/AvatarListView/AvatarListView.tsx
+++
@@ -1,7 +0,0 @@
-import AvatarList, { AvatarListProps } from "@/app/_components/client/AvatarList/AvatarList";
-
-const AvatarListView = (props: AvatarListProps) => {
-  return <AvatarList {...props} />;
-};
-
-export default AvatarListView;
