9b69c11a25dff595f0f2f0233755918b68cac94e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 14:47:25 +0900

    🎨style: TimeStamp 스타일 조정

--- src/app/_components/server/Timeline/components/TimelineItem/components/TimeStamp/TimeStamp.tsx
+++ src/app/_components/server/Timeline/components/TimelineItem/components/TimeStamp/TimeStamp.tsx
@@ -1,19 +1,10 @@
-import { TIMELINE_ITEM_HEIGHT } from "../../../../Timeline.constants";
-
-import { twJoin } from "tailwind-merge";
-
 interface TimeStampProps {
   children: React.ReactNode;
 }
 
 const TimeStamp = ({ children }: TimeStampProps) => {
   return (
-    <div
-      className={twJoin(
-        "flex w-[5rem] items-center justify-center pl-[0.5rem]",
-        TIMELINE_ITEM_HEIGHT,
-      )}
-    >
+    <div className="flex w-[5rem] items-center justify-center pl-[1rem] h-[5.7rem]">
       <p className="flex h-[2.4rem] w-[4rem] items-center justify-center rounded-[0.5rem] text-size12 shadow-example_shadow">
         {children}
       </p>
