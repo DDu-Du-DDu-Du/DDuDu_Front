99eb2016529305bc15a136010e9bb29eb232438b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-10 14:53:40 +0900

    🎨style: Line 점섬 구현

--- src/app/_components/server/Timeline/components/LineBox/LineBox.tsx
+++ src/app/_components/server/Timeline/components/LineBox/LineBox.tsx
@@ -1,7 +1,7 @@
 const LineBox = () => {
   return (
     <div className=" absolute left-[5rem] flex h-full w-[5rem] justify-center">
-      <hr className="h-full w-[0.3rem] bg-example_gray_300" />
+      <hr className="h-full w-[0.3rem] relative before:content-[''] before:absolute before:left-[50%] before:translate-x-[-50%] before:h-[100%] before:w-[0.3rem] border-l-[0.3rem] border-dashed" />
     </div>
   );
 };
