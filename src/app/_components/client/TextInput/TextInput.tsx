211e8a9e109fd6dc3e93b5a31e7e310bb76e2d53
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-11 22:28:24 +0900

    🎨style: Error에 대한 스타일

--- src/app/_components/client/TextInput/TextInput.tsx
+++ src/app/_components/client/TextInput/TextInput.tsx
@@ -11,14 +11,16 @@ interface TextInputProps extends Omit<HTMLAttributes<HTMLInputElement>, "type" |
 }
 
 const TextInput = ({ name, options = {}, ...rest }: TextInputProps) => {
-  const { register } = useFormContext();
+  const { register, formState } = useFormContext();
 
   return (
     <input
       type="text"
       className={twMerge(
         "w-full h-[5.6rem] bg-example_gray_100 rounded-radius15 px-[1.2rem] text-size15",
-        "focus:outline-double focus:outline-example_gray_900 focus:outline-[0.3rem]",
+        !formState.errors[name] &&
+          "focus:outline-double focus:outline-example_gray_900 focus:outline-[0.4rem]",
+        formState.errors[name] && "outline-double outline-example_red_500 outline-[0.4rem]",
       )}
       {...rest}
       {...register(name, options)}
