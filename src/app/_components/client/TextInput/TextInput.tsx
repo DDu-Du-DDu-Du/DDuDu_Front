7b22ce17dc552a08b3a5e49d28b8690a3071bcb0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-11 18:37:50 +0900

    💎feat: TextInput linked useFormContext

--- src/app/_components/client/TextInput/TextInput.tsx
+++ src/app/_components/client/TextInput/TextInput.tsx
@@ -1,7 +1,21 @@
 "use client";
 
-const TextInput = () => {
-  return <div>TextInput</div>;
+import { useFormContext } from "react-hook-form";
+
+interface TextInputProps {
+  name: string;
+}
+
+const TextInput = ({ name }: TextInputProps) => {
+  const { register } = useFormContext();
+
+  return (
+    <input
+      type="text"
+      className="border-2 border-red-200"
+      {...register(name)}
+    />
+  );
 };
 
 export default TextInput;
