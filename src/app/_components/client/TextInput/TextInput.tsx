1e8d46fd1207fae8dceb43f54b5abf6019b3185f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-11 22:43:48 +0900

    💎feat: disabled placeholder 옵션 추가

--- src/app/_components/client/TextInput/TextInput.tsx
+++ src/app/_components/client/TextInput/TextInput.tsx
@@ -8,19 +8,33 @@ import { twMerge } from "tailwind-merge";
 interface TextInputProps extends Omit<HTMLAttributes<HTMLInputElement>, "type" | "onChange"> {
   name: string;
   options?: RegisterOptions<FieldValues, string>;
+  placeholder?: string;
+  disabled?: boolean;
 }
 
-const TextInput = ({ name, options = {}, ...rest }: TextInputProps) => {
+const TextInput = ({
+  name,
+  options = {},
+  className,
+  placeholder,
+  disabled,
+  ...rest
+}: TextInputProps) => {
   const { register, formState } = useFormContext();
 
   return (
     <input
       type="text"
+      placeholder={placeholder}
+      disabled={disabled}
       className={twMerge(
         "w-full h-[5.6rem] bg-example_gray_100 rounded-radius15 px-[1.2rem] text-size15",
         !formState.errors[name] &&
-          "focus:outline-double focus:outline-example_gray_900 focus:outline-[0.4rem]",
-        formState.errors[name] && "outline-double outline-example_red_500 outline-[0.4rem]",
+          "focus:outline-none focus:border-example_gray_900 focus:border-[0.3rem] focus:border-double",
+        formState.errors[name] &&
+          "focus:outline-none border-double border-example_red_500 border-[0.4rem]",
+        disabled && " cursor-default opacity-40",
+        className,
       )}
       {...rest}
       {...register(name, options)}
