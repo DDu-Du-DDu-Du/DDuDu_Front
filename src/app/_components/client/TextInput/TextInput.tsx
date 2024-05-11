4b936b92797fd666d5b4233fe46ec74ab9420058
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-11 21:54:21 +0900

    💎feat: Input style 지정

--- src/app/_components/client/TextInput/TextInput.tsx
+++ src/app/_components/client/TextInput/TextInput.tsx
@@ -2,6 +2,8 @@
 
 import { useFormContext } from "react-hook-form";
 
+import { twMerge } from "tailwind-merge";
+
 interface TextInputProps {
   name: string;
 }
@@ -12,7 +14,10 @@ const TextInput = ({ name }: TextInputProps) => {
   return (
     <input
       type="text"
-      className="border-2 border-red-200"
+      className={twMerge(
+        "w-full h-[5.6rem] bg-example_gray_100 rounded-radius15 px-[1.2rem] text-size15",
+        "focus:outline-double focus:outline-example_gray_900 focus:outline-[0.3rem]",
+      )}
       {...register(name)}
     />
   );
