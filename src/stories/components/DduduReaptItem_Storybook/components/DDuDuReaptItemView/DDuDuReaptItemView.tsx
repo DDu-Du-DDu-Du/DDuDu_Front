ea99f8095203ca44458fe22924a3f8e14bbb4b1f
seunghyun <tmdgus7820@naver.com>
2024-05-13 01:00:35 +0900

    💎feat: DDuDuReaptItem Storybook 추가

---
+++ src/stories/components/DduduReaptItem_Storybook/components/DDuDuReaptItemView/DDuDuReaptItemView.tsx
@@ -0,0 +1,8 @@
+import { DDuDuReaptItem } from "@/app/_components/server";
+import { DDuDuReaptItemProps } from "@/app/_components/server/DDuDuReaptItem/DDuDuReaptItem";
+
+const DDuDuReaptItemView = (props: DDuDuReaptItemProps) => {
+  return <DDuDuReaptItem {...props} />;
+};
+
+export default DDuDuReaptItemView;
---
+++ src/stories/components/DduduReaptItem_Storybook/components/index.ts
@@ -0,0 +1,1 @@
+export { default as DDuDuReaptItemView } from "./DDuDuReaptItemView/DDuDuReaptItemView";
