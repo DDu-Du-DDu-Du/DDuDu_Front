f71eda1e69157c768ac4d581c9fa52714786683b
seunghyun <tmdgus7820@naver.com>
2024-05-14 00:09:22 +0900

    💎feat: NotificationTimeReminder 컴포넌트 제작 - 해당 컴포넌트는 시간 알림 설정 시 해당 날짜, 시간에 리마인더 알림을 보여주는 컴포넌트입니다. - todoId, todoTitle, todoDate를 string 형태로 전달받습니다.

---
+++ src/app/_components/server/NotificationTimeReminder/NotificationTimeReminder.tsx
@@ -0,0 +1,30 @@
+import { getTimeAge } from "./utils";
+
+import Link from "next/link";
+
+export interface NotificationTimeReminderProps {
+  todoId: string;
+  todoTitle: string;
+  todoDate: string;
+}
+
+const NotificationTimeReminder = ({
+  todoId,
+  todoTitle,
+  todoDate,
+}: NotificationTimeReminderProps) => {
+  return (
+    <li className="list-none">
+      <Link
+        className="block rounded-radius15 bg-example_gray_100 px-[1.4rem] py-[1.6rem]"
+        href={`/todo/${todoId}`}
+        title={`${todoTitle} 바로가기`}
+      >
+        <strong className="block text-size13 leading-[1.3rem] font-regular">{todoTitle}</strong>
+        <span className="text-size11 font-light">{getTimeAge(todoDate)}</span>
+      </Link>
+    </li>
+  );
+};
+
+export default NotificationTimeReminder;
---
+++ src/app/_components/server/NotificationTimeReminder/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as getTimeAge } from "./getTimeAgo/getTimeAge";
