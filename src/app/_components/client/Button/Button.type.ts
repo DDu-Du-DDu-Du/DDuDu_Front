3bdc652b4a88cf5a33e201c8a18b9f8e702f06d7
하송희 <99heesong@naver.com>
2024-05-13 12:12:20 +0900

    💎feat: pr 리뷰 반영 (임시 컬러 코드, 커스텀 변수로 수정)

--- src/app/_components/client/Button/Button.type.ts
+++ src/app/_components/client/Button/Button.type.ts
@@ -2,12 +2,12 @@ import React from "react";
 
 import { HTMLMotionProps } from "framer-motion";
 
-export type ButtonRadiusType = "0.94rem" | "0.625rem";
+export type ButtonRadiusType = "basic" | "decreased";
 export type ButtonBorderType = "green" | "none";
 export type ButtonFontSizeType = "0.94rem" | "0.81rem";
 export type ButtonFontWeightType = "regular" | "bold";
 export type ButtonFontColorType = "white" | "black";
-export type ButtonBackgroundColorType = "yellow" | "pink" | "blue";
+export type ButtonBackgroundColorType = "yellow" | "orange" | "red";
 
 export interface ButtonProps extends HTMLMotionProps<"button"> {
   children?: React.ReactNode;
