23896c48b34be5c744559377c9a1033752818a96
seunghyun <tmdgus7820@naver.com>
2024-05-14 00:37:38 +0900

    🎨style: GoalTodoListItem 컴포넌트 style 수정

--- src/app/_components/server/GoalTodoListItem/GoalTodoListItem.tsx
+++ src/app/_components/server/GoalTodoListItem/GoalTodoListItem.tsx
@@ -11,8 +11,8 @@ export interface GoalTodoListItemProps {
 const GoalTodoListItem = ({ title, repeatDays, startDate, endDate }: GoalTodoListItemProps) => {
   return (
     <li className="list-none rounded-radius10 bg-example_gray_100 px-[1.8rem] py-[1.2rem] font-regular">
-      <strong className="mb-[0.3rem] block text-size13">{title}</strong>
-      <span className="mr-[1rem] text-size11 font-light text-example_gray_900">{repeatDays}</span>
+      <strong className="block text-size13 leading-[1.3rem] font-regular">{title}</strong>
+      <span className="mr-[0.5rem] text-size11 font-light text-example_gray_900">{repeatDays}</span>
       <span className="text-size11 font-light text-example_gray_900">
         {formatDateRange(startDate, endDate)}
       </span>
