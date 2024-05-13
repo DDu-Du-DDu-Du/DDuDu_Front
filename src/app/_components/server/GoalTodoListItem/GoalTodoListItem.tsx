237d6770eb5108d52140632e3843cf1ab1eb4348
seunghyun <tmdgus7820@naver.com>
2024-05-13 16:33:48 +0900

    🎨style: GoalTodoListItem에 대한 기본 style 변경

--- src/app/_components/server/GoalTodoListItem/GoalTodoListItem.tsx
+++ src/app/_components/server/GoalTodoListItem/GoalTodoListItem.tsx
@@ -1,5 +1,6 @@
 import formatDateRange from "@/app/_utils/formatDateRange/formatDateRange";
 
+// repeatDays 타입에 대한 Response 값에 따라 추후 변경 필요할 수도 있음
 export interface GoalTodoListItemProps {
   title: string;
   repeatDays: string;
@@ -9,7 +10,7 @@ export interface GoalTodoListItemProps {
 
 const GoalTodoListItem = ({ title, repeatDays, startDate, endDate }: GoalTodoListItemProps) => {
   return (
-    <li className="list-none rounded-radius10 bg-[#F5F5F5] px-[1.8rem] py-[1.2rem] font-regular">
+    <li className="list-none rounded-radius10 bg-example_gray_100 px-[1.8rem] py-[1.2rem] font-regular">
       <strong className="mb-[0.3rem] block text-size13">{title}</strong>
       <span className="mr-[1rem] text-size11 font-light text-example_gray_900">{repeatDays}</span>
       <span className="text-size11 font-light text-example_gray_900">
