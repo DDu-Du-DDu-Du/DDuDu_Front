d3d5a0e6ae30a3189fd4645d85425efad8284101
seunghyun <tmdgus7820@naver.com>
2024-05-14 00:06:42 +0900

    💎feat: getTimeAge 유틸 함수 제작 - ISO Date를 string으로 전달받아 현재 시간 기준으로 며칠 전인지 오전오후인 지 시간이 몇 시 몇 분인지를 보여주는 함수입니다. - 오늘 날짜의 경우 오늘로 표시되며, 어제의 경우는 어제로 표시됩니다.

---
+++ src/app/_components/server/NotificationTimeReminder/utils/getTimeAgo/getTimeAge.tsx
@@ -0,0 +1,25 @@
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
+const getTimeAge = (inputDate: string) => {
+  const currentDate = dayjs();
+  const targetDate = dayjs(inputDate);
+
+  if (dayjs(inputDate).isAfter(currentDate)) {
+    throw new Error("전달된 시간이 현재 시간보다 미래일 수 없습니다.");
+  }
+
+  if (targetDate.isSame(currentDate, "day")) {
+    return `오늘 ${targetDate.format("A h:mm")}`;
+  } else if (targetDate.isSame(currentDate.subtract(1, "day"), "day")) {
+    return `어제 ${targetDate.format("A h:mm")}`;
+  }
+
+  return `${targetDate.from(currentDate).replace(/\s+/g, "")} ${targetDate.format("A h:mm")}`;
+};
+
+export default getTimeAge;
