12aaf9702b9f6e71799b0f9a3d5daf6367dc0e89
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-14 14:14:52 +0900

    🔨chore: Font 변경에 따른 변수 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,12 +57,11 @@ const config = {
       },
 
       fontWeight: {
+        thin: "300",
         regular: "400",
         medium: "500",
         semiBold: "600",
         bold: "700",
-        extraBold: "800",
-        black: "900",
       },
 
       boxShadow: {
