09cfbfcc172f15cf8e0698318396b353d7f7dbd6
seunghyun <tmdgus7820@naver.com>
2024-05-14 10:53:34 +0900

    🎨style: Avatar Storybook 수정 - 타입 추가 및 설명, Viewport 변경

--- src/stories/components/Avatar_Storybook/components/AvatarView/AvatarView.tsx
+++ src/stories/components/Avatar_Storybook/components/AvatarView/AvatarView.tsx
@@ -2,7 +2,11 @@ import { Avatar } from "@/app/_components/client";
 import { AvatarProps } from "@/app/_components/client/Avatar/Avatar";
 
 const AvatarView = (props: AvatarProps) => {
-  return <Avatar {...props} />;
+  return (
+    <div style={{ padding: "50px 0" }}>
+      <Avatar {...props} />
+    </div>
+  );
 };
 
 export default AvatarView;
