02d0a07cd6d4c51783dea58c49405054312394b6
Jaewoong Hwang <noa9925@gmail.com>
2024-05-16 12:18:10 +0900

    🪄change: Storybook 주석 수정

--- src/stories/components/BottomMultipleCalender_Storybook/BottomMultipleCalender.stories.tsx
+++ src/stories/components/BottomMultipleCalender_Storybook/BottomMultipleCalender.stories.tsx
@@ -7,8 +7,8 @@ import type { Meta, StoryObj } from "@storybook/react";
  * ## BottomMultipleCalender Component
  *
  * ### Props
- * - **selected : **Bottom Sheet 내부에 전달됩니다.
- * - **setSelected : **Bottom Sheet가 오픈될지에 대한 `boolean`값을 전달받습니다.
+ * - **selected : **캘린더에서 선택한 날짜 데이터들을 담고 있는 State입니다.
+ * - **setSelected : **선택한 날짜 데이터를 상위 요소에 전달하는 Setter입니다.
  *
  * 캘린더를 사용하고자 하는 영역에서 useState를 통해 state와 setter를 연결해주세요!
  * */
@@ -27,7 +27,7 @@ const meta = {
 
     setSelected: {
       control: { disable: true },
-      description: "Bottom Sheet가 오픈될지에 대한 `boolean`값을 전달받습니다.",
+      description: "선택한 날짜 데이터를 상위 요소에 전달하는 Setter입니다.",
     },
   },
   args: {},
