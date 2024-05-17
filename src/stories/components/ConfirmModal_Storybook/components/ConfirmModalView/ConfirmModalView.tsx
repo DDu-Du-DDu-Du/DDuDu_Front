4526185d6870bd4c7b4d3bc8efa321ea6c7d9715
seunghyun <tmdgus7820@naver.com>
2024-05-17 17:25:24 +0900

    💎feat: ConfirmModal Storybook 추가

---
+++ src/stories/components/ConfirmModal_Storybook/components/ConfirmModalView/ConfirmModalView.tsx
@@ -0,0 +1,8 @@
+import { ConfirmModal } from "@/app/_components/client";
+import { ConfirmModalProps } from "@/app/_components/client/ConfirmModal/ConfirmModal";
+
+const ConfirmModalView = (props: ConfirmModalProps) => {
+  return <ConfirmModal {...props} />;
+};
+
+export default ConfirmModalView;
