30d73e7413214f63ad56388b9526e63c857136e8
Jaewoong Hwang <noa9925@gmail.com>
2024-05-21 12:15:54 +0900

    💎feat: DailyDDuDu 컴포넌트 작업

---
+++ src/app/_components/client/Calender/FeedCalender/components/DailyDDuDu/DailyDDuDu.constant.ts
@@ -0,0 +1,9 @@
+export const DAILY_DDUDU_MOCK_DATA = {
+  "2024-05-1": { total: 120, done: 4, rest: 116 },
+  "2024-05-3": { total: 20, done: 7, rest: 13 },
+  "2024-05-4": { total: 20, done: 10, rest: 10 },
+  "2024-05-9": { total: 20, done: 12, rest: 8 },
+  "2024-05-12": { total: 20, done: 14, rest: 6 },
+  "2024-05-21": { total: 20, done: 15, rest: 5 },
+  "2024-05-25": { total: 20, done: 20, rest: 0 },
+};
