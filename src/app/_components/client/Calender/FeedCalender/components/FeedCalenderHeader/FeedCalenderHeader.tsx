ac549962d14ad5f5cc209c16c78dc83c1ef73d8b
Jaewoong Hwang <noa9925@gmail.com>
2024-05-21 23:56:05 +0900

    🪄change: FeedCalender 내부 컴포넌트 분리 및 Today 처리

--- src/app/_components/client/Calender/FeedCalender/components/FeedCalenderHeader/FeedCalenderHeader.tsx
+++ src/app/_components/client/Calender/FeedCalender/components/FeedCalenderHeader/FeedCalenderHeader.tsx
@@ -5,7 +5,7 @@ import { CaptionProps, useNavigation } from "react-day-picker";
 import ChevronLeftIcon from "@/app/_components/server/icons/ChevronLeftIcon/ChevronLeftIcon";
 import ChevronRightIcon from "@/app/_components/server/icons/ChevronRightIcon/ChevronRightIcon";
 
-const FeedCalenderHeader = (props: CaptionProps, MonthlyGoal: string[]) => {
+const FeedCalenderHeader = (props: CaptionProps, MonthlyGoal?: string[]) => {
   const { goToMonth, nextMonth, previousMonth } = useNavigation();
   const currentYear = props.displayMonth.getFullYear();
   const currentMonth = props.displayMonth.getMonth() + 1;
@@ -16,7 +16,7 @@ const FeedCalenderHeader = (props: CaptionProps, MonthlyGoal: string[]) => {
         <header className="text-lg font-medium">
           {currentYear}년 {currentMonth}월
         </header>
-        <div className="flex items-center">
+        <article className="flex items-center">
           <button
             onClick={() => previousMonth && goToMonth(previousMonth)}
             className="h-5 w-5"
@@ -29,15 +29,20 @@ const FeedCalenderHeader = (props: CaptionProps, MonthlyGoal: string[]) => {
           >
             <ChevronRightIcon className="h-5 w-5 fill-example_gray_1200" />
           </button>
-        </div>
+        </article>
       </section>
-      <div className="bg-example_gray_100 w-full rounded-radius10 flex justify-start items-start p-3 text-sm mb-4">
-        <ul>
-          {MonthlyGoal.map((goal, index) => (
-            <li key={index}>{goal}</li>
-          ))}
-        </ul>
-      </div>
+      <article className="bg-example_gray_100 w-full rounded-radius10 flex justify-start items-start p-3 text-sm mb-4">
+        {MonthlyGoal ? (
+          <ul>
+            {MonthlyGoal.map((goal, index) => (
+              <li key={index}>{goal}</li>
+            ))}
+          </ul>
+        ) : (
+          // TODO: 추후 디자인, 로직 수정
+          <p className="w-full text-center text-example_gray_700">월간 목표 추가하기</p>
+        )}
+      </article>
     </header>
   );
 };
