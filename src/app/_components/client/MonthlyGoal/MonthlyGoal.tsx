aeb0e25b39b5e5f41660048a79fe994662bb3a8f
하송희 <99heesong@naver.com>
2024-05-23 02:04:32 +0900

    💎feat: MonthlyGoal 기본 스타일링, props 설정

--- src/app/_components/client/MonthlyGoal/MonthlyGoal.tsx
+++ src/app/_components/client/MonthlyGoal/MonthlyGoal.tsx
@@ -1,12 +1,22 @@
 "use-client";
 
-const MonthlyGoal = () => {
+import React from "react";
+
+interface MonthlyGoalProps {
+  value: string;
+  onChange: (value: string) => void;
+  onBlur?: () => void;
+}
+
+const MonthlyGoal = ({ value, onChange, onBlur }: MonthlyGoalProps) => {
   return (
     <div className="w-[31.2rem] h-[4.6rem] flex justify-center items-center">
-      <input
-        type="text"
+      <textarea
         placeholder="월 별 목표를 설정해보세요!"
-        className="w-full h-full p-4 bg-example_gray_100 focus:outline-none rounded-radius10 text-size11"
+        className="w-full h-full p-4 bg-example_gray_100 focus:outline-none rounded-radius10 text-size11 resize-none"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onBlur={onBlur}
       />
     </div>
   );
