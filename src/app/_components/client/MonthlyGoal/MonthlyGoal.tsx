121383e0fb19491bae5a705c815d88dcff2b23cb
하송희 <99heesong@naver.com>
2024-05-23 04:23:26 +0900

    💎feat: MonthlyGoal 오버레이 구현 (정중앙 기준 60rem)

--- src/app/_components/client/MonthlyGoal/MonthlyGoal.tsx
+++ src/app/_components/client/MonthlyGoal/MonthlyGoal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface MonthlyGoalProps {
   value: string;
@@ -10,6 +10,7 @@ interface MonthlyGoalProps {
 
 const MonthlyGoal = ({ value, onChange, onBlur }: MonthlyGoalProps) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
 
   useEffect(() => {
     if (textareaRef.current) {
@@ -18,16 +19,31 @@ const MonthlyGoal = ({ value, onChange, onBlur }: MonthlyGoalProps) => {
     }
   }, [value]);
 
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    if (onBlur) onBlur();
+  };
+
   // TODO: textarea 세로 중앙 정렬, 최소 높이 4.6rem으로 설정(지금은 넘음)
   return (
-    <div className="w-[31.2rem] h-full flex items-center justify-center">
+    <div className="relative w-[31.2rem] h-full flex items-center justify-center">
+      {isFocused && (
+        <div className="fixed inset-0 flex items-center justify-center z-40">
+          <div className="bg-black_500 bg-opacity-20 w-[60rem] h-full"></div>
+        </div>
+      )}
       <textarea
         ref={textareaRef}
         placeholder="월 별 목표를 설정해보세요!"
-        className="w-full min-h-[4.6rem] p-6 bg-example_gray_100 focus:outline-none rounded-radius10 text-size11 font-medium resize-none leading-[1.85] box-border"
+        className="relative z-50 w-full min-h-[4.6rem] p-6 bg-example_gray_100 focus:outline-none rounded-radius10 text-size11 font-medium resize-none leading-[1.85] box-border"
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        onBlur={onBlur}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
     </div>
   );
