29200bb982c8f90d8ae76d99efb3b21419a46b66
하송희 <99heesong@naver.com>
2024-05-23 03:23:13 +0900

    🎨style: MonthlyGoal - 입력에 따라 높이 변화, 글씨체 변경

--- src/app/_components/client/MonthlyGoal/MonthlyGoal.tsx
+++ src/app/_components/client/MonthlyGoal/MonthlyGoal.tsx
@@ -1,6 +1,6 @@
-"use-client";
+"use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 interface MonthlyGoalProps {
   value: string;
@@ -9,11 +9,22 @@ interface MonthlyGoalProps {
 }
 
 const MonthlyGoal = ({ value, onChange, onBlur }: MonthlyGoalProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight / 10}rem`;
+    }
+  }, [value]);
+
+  // TODO: textarea 세로 중앙 정렬, 최소 높이 4.6rem으로 설정(지금은 넘음)
   return (
-    <div className="w-[31.2rem] h-[4.6rem] flex justify-center items-center">
+    <div className="w-[31.2rem] h-full flex items-center justify-center">
       <textarea
+        ref={textareaRef}
         placeholder="월 별 목표를 설정해보세요!"
-        className="w-full h-full p-4 bg-example_gray_100 focus:outline-none rounded-radius10 text-size11 resize-none"
+        className="w-full min-h-[4.6rem] p-6 bg-example_gray_100 focus:outline-none rounded-radius10 text-size11 font-medium resize-none leading-[1.85] box-border"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         onBlur={onBlur}
