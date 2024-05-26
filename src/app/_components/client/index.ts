21a9bd8660342e1bc481ca0d19ff5c5323213756
seunghyun <tmdgus7820@naver.com>
2024-05-26 23:27:13 +0900

    💎feat: FollowRequestItem 컴포넌트 제작 - 팔로우 요청이 왔을 때 전달받는 리스트 아이템

--- src/app/_components/client/index.ts
+++ src/app/_components/client/index.ts
@@ -17,3 +17,5 @@ export { default as ConfirmModal } from "./ConfirmModal/ConfirmModal";
 export { default as InputDate } from "./InputDate/InputDate";
 export { default as SwitchButton } from "./SwitchButton/SwitchButton";
 export { default as MonthlyGoal } from "./MonthlyGoal/MonthlyGoal";
+
+export { default as FollowRequestItem } from "./FollowRequestItem/FollowRequestItem";
