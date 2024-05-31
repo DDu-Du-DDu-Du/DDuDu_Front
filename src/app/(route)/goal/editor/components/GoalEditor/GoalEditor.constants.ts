42d4a908b7488de5abef47effe42a299d781104e
seunghyun <tmdgus7820@naver.com>
2024-06-01 02:56:14 +0900

    📝docs: GoalEditorPage 데이터 상수 처리 - goalPrivacy 타입 데이터 상수 정의 - todoRepeatList 더미 데이터로 임시 처리

---
+++ src/app/(route)/goal/editor/components/GoalEditor/GoalEditor.constants.ts
@@ -0,0 +1,36 @@
+export const PRIVACY_TYPE = {
+  PUBLIC: "전체 공개",
+  FOLLOWER: "팔로워 공개",
+  PRIVATE: "나만보기",
+};
+
+export const DDUDU_REPEAT_LIST = [
+  {
+    id: 0,
+    title: "일어나자마자 물 한 컵 마시기",
+    repeatDays: "매일",
+    startDate: "2024-05-10",
+    endDate: "2024-05-13",
+  },
+  {
+    id: 1,
+    title: "학원 특강",
+    repeatDays: "화 수",
+    startDate: "2024-05-01",
+    endDate: "2024-05-01",
+  },
+  {
+    id: 2,
+    title: "학원 특강",
+    repeatDays: "화 수",
+    startDate: "2024-05-01",
+    endDate: "2024-05-01",
+  },
+  {
+    id: 3,
+    title: "학원 특강",
+    repeatDays: "화 수",
+    startDate: "2024-05-01",
+    endDate: "2024-05-01",
+  },
+];
