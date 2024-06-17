27e4f073da2872974ecf03ea676a2e9ac701b2e5
Jaewoong Hwang <noa9925@gmail.com>
2024-06-17 18:14:22 +0900

    🚜change: 이후 진행 예정 항목 정리
    - refreshToken을 통한 accessToken 갱신 에러 해결
    - 유저 정보 갱신 후 세션에 저장
    - 전역 스토어를 통한 관리 (고려)

--- src/app/_api/fetchApi/fetchApi.ts
+++ src/app/_api/fetchApi/fetchApi.ts
@@ -1,6 +1,11 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-const fetchApi = (url: string, options: RequestInit = {}) => {
+// TODO requireAuth 파라미터 설정
+const fetchApi = (url: string, options: RequestInit = {}, requiredAuth: boolean = false) => {
+  if (requiredAuth) {
+    // TODO 세션에서 accessToken을 가져와 bearer header에 담아 통신
+  }
+
   return fetch(`${API_BASE_URL}${url}`, {
     ...options,
   });
