f0a7021602099b664c3842f1f40f6e1a467b79ba
Jaewoong Hwang <noa9925@gmail.com>
2024-06-18 17:29:46 +0900

    🚜change: API 엔드포인트 변수 수정

--- src/app/_constants/index.ts
+++ src/app/_constants/index.ts
@@ -1,3 +1,3 @@
 export * from "./query/query";
 export * from "./errorCode/errorCode";
-export * from "./endPoint/endPoint";
+export * from "./endPoint";
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
 // const matchersForAuth = ["/dashboard/:path*", "/myaccount/:path*", "/settings/:path*", "..."];
 
-export async function middleware(request: NextRequest) {
-  console.log(request);
+export async function middleware() {
+  // console.log(request);
 
   return NextResponse.next();
 }
