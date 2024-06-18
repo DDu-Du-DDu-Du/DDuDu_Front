0936baac7c71b974d82c3e9488aa2e30f27e766f
seunghyun <tmdgus7820@naver.com>
2024-06-18 19:10:27 +0900

    💎feat: GoalStatistics 컴포넌트 제작 - GoalItem의 type이 statistic인 경우 보여지는 컴포넌트 - 목표 통계 페이지의 목표 아이템

---
+++ src/app/_components/server/GoalItem/components/GoalStatistics/GoalStatistics.tsx
@@ -0,0 +1,29 @@
+import { ChevronRightIcon } from "@/app/_components/server";
+
+import Link from "next/link";
+
+interface GoalStatisticsProps {
+  id: number;
+  goalName: string;
+}
+
+const GoalStatistics = ({ id, goalName }: GoalStatisticsProps) => {
+  return (
+    <div className="flex justify-between items-center mb-[2.8rem]">
+      <strong className="inline-block py-[0.9rem] px-[1.9rem] bg-example_gray_100 rounded-radius15 select-none">
+        {goalName}
+      </strong>
+      <Link
+        href={`/statistics/goal/${id}`}
+        title="목표 통계 페이지"
+      >
+        <ChevronRightIcon
+          size={16}
+          fill="#D9D9D9"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default GoalStatistics;
