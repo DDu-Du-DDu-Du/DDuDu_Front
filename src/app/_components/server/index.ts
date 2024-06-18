225efc87c3c12ab448da36a801d0ea38e850a410
seunghyun <tmdgus7820@naver.com>
2024-06-18 19:14:18 +0900

    💎feat: GoalItem 컴포넌트 제작

--- src/app/_components/server/index.ts
+++ src/app/_components/server/index.ts
@@ -2,6 +2,7 @@ export * from "./icons";
 
 export { default as ExampleIcon } from "./ExampleIcon/ExampleIcon";
 export { default as GoalTodoListItem } from "./GoalTodoListItem/GoalTodoListItem";
+export { default as GoalItem } from "./GoalItem/GoalItem";
 
 export { default as Timeline } from "./Timeline/Timeline";
 export { default as NotificationTimeReminder } from "./NotificationTimeReminder/NotificationTimeReminder";
