c74348b72f9914c80c7c649affb8fc9b8bf6987b
seunghyun <tmdgus7820@naver.com>
2024-06-20 23:54:37 +0900

    💎feat: Goal 페이지 작업 - 전역 z-index 추가 (header right button으로 들어가는 경우 사용될 z-index)

--- src/app/(route)/goal/page.tsx
+++ src/app/(route)/goal/page.tsx
@@ -1,5 +1,22 @@
+import { PlusIcon } from "@/app/_components/server";
+
+import { GoalDragDrop } from "./components";
+
+import Link from "next/link";
+
 const GoalPage = () => {
-  return <div>GoalPage</div>;
+  return (
+    <div className="relative pt-[2.6rem] px-[2.4rem]">
+      <Link
+        className="absolute -top-[3.4rem] right-[2.4rem] z-headerLink"
+        href="/goal/editor/create"
+        title="목표 생성하기"
+      >
+        <PlusIcon />
+      </Link>
+      <GoalDragDrop />
+    </div>
+  );
 };
 
 export default GoalPage;
