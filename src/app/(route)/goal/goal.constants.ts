90b2b9b43f9aa8db4daba267c99060853016f717
seunghyun <tmdgus7820@naver.com>
2024-06-20 23:34:51 +0900

    📝docs: 목표 관리 페이지 더미 데이터 추가

---
+++ src/app/(route)/goal/goal.constants.ts
@@ -0,0 +1,32 @@
+export const GOAL_DUMMY_DATA = [
+  {
+    id: 0,
+    name: "테스트1",
+    status: "DONE",
+    color: "#000000",
+  },
+  {
+    id: 1,
+    name: "테스트2",
+    status: "DONE",
+    color: "#000000",
+  },
+  {
+    id: 2,
+    name: "테스트3",
+    status: "DONE",
+    color: "#000000",
+  },
+  {
+    id: 3,
+    name: "테스트4",
+    status: "DONE",
+    color: "#000000",
+  },
+  {
+    id: 4,
+    name: "테스트5",
+    status: "DONE",
+    color: "#000000",
+  },
+];
