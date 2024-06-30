77bdc6b515c54f5277f0c264de37cb4fde84a297
seunghyun <tmdgus7820@naver.com>
2024-07-01 02:15:51 +0900

    🛠️refactor: DDuDuSheet에 바로 닫히는 props 속성 추가 - Sheet를 열고 닫을 수 있는 상태 값을 닫게 하는 속성 전달

--- src/app/_components/client/DDuDuSheet/DDuDuSheet.tsx
+++ src/app/_components/client/DDuDuSheet/DDuDuSheet.tsx
@@ -1,11 +1,17 @@
 import { BottomSheet } from "../BottomSheet";
 import { DDuDuMenu } from "./components";
 
-const DDuDuSheet = () => {
+interface DDuDuSheetProps {
+  onClose: () => void;
+}
+
+const DDuDuSheet = ({ onClose }: DDuDuSheetProps) => {
   return (
     <BottomSheet
       isShow
       defaultHeight={"fit-content"}
+      maxHeight={"fit-content"}
+      onClose={onClose}
     >
       <DDuDuMenu />
     </BottomSheet>
