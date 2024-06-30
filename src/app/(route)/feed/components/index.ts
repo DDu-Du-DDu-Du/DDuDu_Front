6430ff696a83dca734e9cac9f1c628d7ea6e88df
seunghyun <tmdgus7820@naver.com>
2024-07-01 02:13:25 +0900

    📝docs: Main 페이지의 스케줄(타임라인) 페이지 생성

---
+++ src/app/(route)/feed/components/index.ts
@@ -0,0 +1,4 @@
+export { default as MainHeader } from "./MainHeader/MainHeader";
+export { default as MainFeed } from "./MainFeed/MainFeed";
+export { default as MainSchedule } from "./MainSchedule/MainSchedule";
+export { default as MainGoalItem } from "./MainGoalItem/MainGoalItem";
