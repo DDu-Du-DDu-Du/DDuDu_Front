56011e73c6e9d7d4df4cd31a4212863d2a891602
seunghyun <tmdgus7820@naver.com>
2024-07-01 02:09:54 +0900

    💎feat: GoalItem 컴포넌트에 GoalCreateDDuDu 컴포넌트 추가 - 기존 컴포넌트 props 변경 - 새로운 type GoalCreateDDuDu 컴포넌트 추가

--- src/app/_components/server/GoalItem/components/index.ts
+++ src/app/_components/server/GoalItem/components/index.ts
@@ -1,2 +1,3 @@
 export { default as GoalManagement } from "./GoalManagement/GoalManagement";
 export { default as GoalStatistics } from "./GoalStatistics/GoalStatistics";
+export { default as GoalCreateDDuDu } from "./GoalCreateDDuDu/GoalCreateDDuDu";
