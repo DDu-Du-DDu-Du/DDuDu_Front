92f76dcde912c992870ebe30f8026cc910adf98b
seunghyun <tmdgus7820@naver.com>
2024-07-01 02:14:43 +0900

    💎feat: Main 페이지 전체적인 구조 작업 - 투두 작업 및 스케줄 페이지 분리

--- src/app/(route)/feed/page.tsx
+++ src/app/(route)/feed/page.tsx
@@ -1,5 +1,19 @@
-const FeedPage = () => {
-  return <div>FeedPage</div>;
+import { MainFeed, MainHeader, MainSchedule } from "./components";
+
+interface FeedPageProps {
+  searchParams: { view: string };
+}
+
+const FeedPage = ({ searchParams }: FeedPageProps) => {
+  const { view } = searchParams;
+
+  return (
+    <div className="py-[2.4rem] px-[2.4rem]">
+      <MainHeader />
+      {view === "ddudu" && <MainFeed />}
+      {view === "schedule" && <MainSchedule />}
+    </div>
+  );
 };
 
 export default FeedPage;
