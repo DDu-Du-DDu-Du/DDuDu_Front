37a4969a3a0e6779aecc2e329378666a747c0cb1
seunghyun <tmdgus7820@naver.com>
2024-07-12 13:52:23 +0900

    🛠️refactor: Calendar 컴포넌트 수정 - 날짜 클릭 시 클릭한 날짜를 쿼리 스트링으로 관리할 수 있도록 수정 - 전체적인 스타일 수정 - 데이터 타입에 대한 수정

--- src/app/_components/client/Calender/FeedCalender/components/FeedCalenderDayConetent/FeedCalenderDayContent.tsx
+++ src/app/_components/client/Calender/FeedCalender/components/FeedCalenderDayConetent/FeedCalenderDayContent.tsx
@@ -6,28 +6,44 @@ import useFeedCalenderDayConetent from "../../hooks/useFeedCalenderDayContent/us
 import DailyDDuDu from "../DailyDDuDu/DailyDDuDu";
 import { DAILY_DDUDU_MOCK_DATA_TYPE } from "../DailyDDuDu/DailyDDuDu.constant";
 
+import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
-const FeedCalenderDayContent = (
-  props: DayContentProps,
-  monthlyDDuDus: DAILY_DDUDU_MOCK_DATA_TYPE,
-) => {
+interface FeedCalenderDayContentProps {
+  props: DayContentProps;
+  monthlyDDuDus: DAILY_DDUDU_MOCK_DATA_TYPE;
+  currentURL: string;
+  selectedDDuDu?: string;
+}
+
+const FeedCalenderDayContent = ({
+  props,
+  monthlyDDuDus,
+  currentURL,
+  selectedDDuDu,
+}: FeedCalenderDayContentProps) => {
   const { isToday, selectedDate, formattedDate } = useFeedCalenderDayConetent(props);
+
+  const router = useRouter();
 
   return (
     <div
       className={twMerge(
-        "flex flex-col gap-2",
-        isToday && "font-bold bg-example_gray_300 rounded-radius5",
+        "flex flex-col items-center justify-center gap-2 w-[4.2rem] h-[5.4rem] text-size11",
+        (selectedDDuDu === formattedDate || (!selectedDDuDu && isToday)) &&
+          "font-bold bg-example_gray_300 rounded-radius5",
       )}
+      onClick={() => router.replace(`${currentURL}&date=${formattedDate}`)}
     >
       {/* TODO: API 연결 후 데이터 교체 */}
-      {monthlyDDuDus[formattedDate] && (
+      {monthlyDDuDus[formattedDate] ? (
         <DailyDDuDu
           totalCount={monthlyDDuDus[formattedDate].total}
           doneCount={monthlyDDuDus[formattedDate].done}
           restCount={monthlyDDuDus[formattedDate].rest}
         />
+      ) : (
+        <div className="rounded-full shrink-0 w-[2.4rem] h-[2.4rem] bg-example_gray_100 flex justify-center items-center" />
       )}
 
       <p className="text-example_gray_800">{selectedDate}</p>
