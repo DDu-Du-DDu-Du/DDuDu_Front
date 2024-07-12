c2e2fe2f419ee2eda7296563053023570d2c27f3
seunghyun <tmdgus7820@naver.com>
2024-07-12 13:29:34 +0900

    📝docs: 메인 페이지(feed)의 데이터 type 정의

--- src/app/(route)/feed/feed.types.ts
+++ src/app/(route)/feed/feed.types.ts
@@ -10,7 +10,22 @@ export type MainDDuDusType = {
   status: "COMPLETE" | "UNCOMPLETED";
 };
 
-export interface MainGoalItemType {
+export interface MainDailyListType {
   goal: MainGoalType;
   ddudus: MainDDuDusType[];
 }
+
+export interface MainDailyTimeTableType {
+  timetable: MainTimeTableType[];
+  unassignedDdudus: MainDailyListType[];
+}
+
+export type MainTimeTableType = {
+  beginAt: string;
+  ddudus: {
+    id: number;
+    name: string;
+    status: "COMPLETE" | "UNCOMPLETED";
+    goalId: number;
+  }[];
+};
