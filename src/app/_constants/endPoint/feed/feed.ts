0c6d7d3b5e129b7b6a8477f80a72f6ded0bbe320
seunghyun <tmdgus7820@naver.com>
2024-07-12 13:39:34 +0900

    📝docs: 메인 페이지(feed)에 대한 API URL,  fetch 함수 정의

---
+++ src/app/_constants/endPoint/feed/feed.ts
@@ -0,0 +1,12 @@
+const FEED = {
+  DAILY_LIST: "/api/ddudus/daily/list",
+  DAILY_TIMETABLE: "/api/ddudus/daily/timetable",
+  CREATE_DDUDU: "/api/ddudus",
+  EDIT_DDUDU: "/api/ddudus",
+  DELETE_DDUDU: "/api/ddudus",
+  COMPLETE_TOGGLE_DDUDU: "/api/ddudus",
+  MONTHLY_GOALS: "/api/period-goals",
+  DDUDU_DETAIL: "/api/ddudus",
+};
+
+export default FEED;
--- src/app/_constants/endPoint/index.ts
+++ src/app/_constants/endPoint/index.ts
@@ -1,3 +1,4 @@
 export { default as AUTH } from "./auth/auth";
 export { default as USER } from "./user/user";
 export { default as GOAL } from "./goal/goal";
+export { default as FEED } from "./feed/feed";
