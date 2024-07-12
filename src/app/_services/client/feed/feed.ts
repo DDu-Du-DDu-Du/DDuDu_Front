8b74ca19022e6579a801966a9ebcb826a7c7d881
seunghyun <tmdgus7820@naver.com>
2024-07-12 13:39:34 +0900

    📝docs: 메인 페이지(feed)에 대한 API URL,  fetch 함수 정의

---
+++ src/app/_services/client/feed/feed.ts
@@ -0,0 +1,157 @@
+import { fetchApi } from "@/app/_api";
+import { FEED } from "@/app/_constants";
+
+export const getDailyList = async (accessToken: string, userId: number, date: string) => {
+  const selectedDate = date ? `&date=${date}` : "";
+
+  const response = await fetchApi(`${FEED.DAILY_LIST}?userId=${userId}${selectedDate}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const getDailyTimeTable = async (accessToken: string, userId: number) => {
+  const response = await fetchApi(`${FEED.DAILY_TIMETABLE}?userId=${userId}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface GetMonthlyGoalsProps {
+  accessToken: string;
+  type: "WEEK" | "MONTH";
+}
+
+export const getMonthlyGoals = async ({ accessToken, type }: GetMonthlyGoalsProps) => {
+  const response = await fetchApi(`${FEED.MONTHLY_GOALS}?type=${type}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      header: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const getDDuDuDetail = async ({ accessToken, id }: FetchUpdateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.DDUDU_DETAIL}/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      header: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchCreateDDuDuProps {
+  accessToken: string;
+  requestDDuDu: {
+    goalId: number;
+    name: string;
+    scheduledOn: string;
+  };
+}
+
+export const fetchCreateDDuDu = async ({ accessToken, requestDDuDu }: FetchCreateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.CREATE_DDUDU}`, {
+    method: "POST",
+    body: JSON.stringify(requestDDuDu),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchEditDDuDuProps {
+  accessToken: string;
+  id: number;
+  name: string;
+}
+
+export const fetchEditDDuDu = async ({ accessToken, id, name }: FetchEditDDuDuProps) => {
+  const response = await fetchApi(`${FEED.EDIT_DDUDU}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ name }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchUpdateDDuDuProps {
+  accessToken: string;
+  id: number;
+}
+
+export const fetchDeleteDDuDu = async ({ accessToken, id }: FetchUpdateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.DELETE_DDUDU}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const fetchCompleteToggleDDuDu = async ({ accessToken, id }: FetchUpdateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.COMPLETE_TOGGLE_DDUDU}/${id}/status`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+};
