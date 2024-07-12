2d9286b064a9c5129a149ebd0d63ee623c5a5a93
seunghyun <tmdgus7820@naver.com>
2024-07-12 13:31:13 +0900

    💎feat: MainDDuDuInput 컴포넌트 제작 - 목표에 대한 뚜두 생성 시 입력할 수 있는 컴포넌트로 동작합니다. - 입력을 완료하면 뚜두가 생성됩니다.

--- src/app/(route)/feed/components/MainGoalItem/components/MainDDuDuInput/MainDDuDuInput.tsx
+++ src/app/(route)/feed/components/MainGoalItem/components/MainDDuDuInput/MainDDuDuInput.tsx
@@ -1,29 +1,86 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 
+import { MainDDuDusType } from "@/app/(route)/feed/feed.types";
 import { OptionIcon } from "@/app/_components/server/icons";
 import { useClickAway } from "@/app/_hooks";
+import { fetchCreateDDuDu, fetchEditDDuDu } from "@/app/_services/client";
+import formatDateToYYYYMMDD from "@/app/_utils/formatDateToYYYYMMDD/formatDateToYYYYMMDD";
+import { useMutation } from "@tanstack/react-query";
+
+import { useSession } from "next-auth/react";
 
 interface MainDDuDuInput {
+  type?: "create" | "edit";
   goalId: number;
   color: string;
+  dduduItem?: MainDDuDusType;
   onCloseDDuDuInput: (event?: MouseEvent | TouchEvent) => void;
-  onCreateDDuDu: (ddudu: string) => void;
+  onCreateDDuDu?: ({ id, ddudu }: { id: number; ddudu: string }) => void;
+  onEditDDuDu?: (ddudu: MainDDuDusType) => void;
 }
 
 interface DDuDuInputType {
   ddudu: string;
 }
 
-const MainDDuDuInput = ({ goalId, color, onCloseDDuDuInput, onCreateDDuDu }: MainDDuDuInput) => {
-  const { handleSubmit, register, reset } = useForm<DDuDuInputType>();
+const MainDDuDuInput = ({
+  type = "create",
+  goalId,
+  color,
+  dduduItem,
+  onCloseDDuDuInput,
+  onCreateDDuDu,
+  onEditDDuDu,
+}: MainDDuDuInput) => {
+  const { handleSubmit, register, reset, getValues } = useForm<DDuDuInputType>();
+  const { data: session } = useSession();
+
+  const onCreateSuccess = ({ id }: { id: number }) => {
+    const ddudu = getValues("ddudu");
+    onCreateDDuDu && onCreateDDuDu({ id, ddudu });
+    reset();
+  };
+
+  const createDDuDuMutation = useMutation({
+    mutationKey: ["dduduCreate"],
+    mutationFn: fetchCreateDDuDu,
+    onSuccess: onCreateSuccess,
+  });
+
+  const editDDuDuMutation = useMutation({
+    mutationKey: ["dduduEdit"],
+    mutationFn: fetchEditDDuDu,
+  });
 
   const dduduRef = useClickAway<HTMLLIElement>(onCloseDDuDuInput);
 
   const onValid: SubmitHandler<DDuDuInputType> = ({ ddudu }) => {
-    // /api/ddudus 뚜두 생성
-    console.log(goalId, ddudu);
-    onCreateDDuDu(ddudu);
-    reset();
+    const nowDate = formatDateToYYYYMMDD(new Date());
+
+    if (type === "create") {
+      createDDuDuMutation.mutate({
+        accessToken: session?.sessionToken as string,
+        requestDDuDu: {
+          goalId,
+          name: ddudu,
+          scheduledOn: nowDate,
+        },
+      });
+    } else if (type === "edit" && dduduItem) {
+      editDDuDuMutation.mutate({
+        accessToken: session?.sessionToken as string,
+        id: dduduItem.id,
+        name: ddudu,
+      });
+
+      const dduduData = {
+        id: dduduItem.id,
+        name: ddudu,
+        status: dduduItem.status,
+      };
+
+      onEditDDuDu && onEditDDuDu(dduduData);
+    }
   };
 
   return (
@@ -33,7 +90,7 @@ const MainDDuDuInput = ({ goalId, color, onCloseDDuDuInput, onCreateDDuDu }: Mai
     >
       <div
         className=" block size-[2rem] rounded-circle border-solid border-2 bg-[white]  mr-[1rem]"
-        style={{ borderColor: color }}
+        style={{ borderColor: `#${color}` }}
       />
       <form
         className="flex-1"
@@ -41,10 +98,11 @@ const MainDDuDuInput = ({ goalId, color, onCloseDDuDuInput, onCreateDDuDu }: Mai
       >
         <input
           className="w-[100%] py-[0.5rem] px-[0.5rem] border-solid border-b outline-none"
-          style={{ borderColor: color }}
+          style={{ borderColor: `#${color}` }}
           type="text"
           placeholder="할 일 입력"
           autoFocus
+          defaultValue={dduduItem?.name || ""}
           {...register("ddudu", {
             required: true,
           })}
