211487fc081439294421b7732b3e66bca986a387
seunghyun <tmdgus7820@naver.com>
2024-07-14 18:41:48 +0900

    🛠️refactor: BottomSingleCalender 컴포넌트 리팩토링

--- src/app/_components/client/Calender/BottomSingleCalender/BottomSingleCalender.tsx
+++ src/app/_components/client/Calender/BottomSingleCalender/BottomSingleCalender.tsx
@@ -7,43 +7,70 @@ import { CaptionLabelProps, DayPicker } from "react-day-picker";
 import ChevronLeftIcon from "@/app/_components/server/icons/ChevronLeftIcon/ChevronLeftIcon";
 import ChevronRightIcon from "@/app/_components/server/icons/ChevronRightIcon/ChevronRightIcon";
 
+import { BottomSheet } from "../../BottomSheet";
+
 import { ko } from "date-fns/locale/ko";
 
 export interface BottomSingleCalenderProps {
-  selected: Date;
+  selected: Date | undefined;
   setSelected: (dates: Date | undefined) => void;
+  handleCalendarSheetToggleOff: () => void;
 }
 
-const BottomSingleCalender = ({ selected, setSelected }: BottomSingleCalenderProps) => {
+const BottomSingleCalender = ({
+  selected,
+  setSelected,
+  handleCalendarSheetToggleOff,
+}: BottomSingleCalenderProps) => {
+  const handleSelectedDate = () => {
+    handleCalendarSheetToggleOff();
+  };
+
   return (
-    <DayPicker
-      locale={ko}
-      fixedWeeks
-      mode={"single"}
-      selected={selected}
-      onSelect={setSelected}
-      className="w-full"
-      classNames={BottomSheetCalenderStyles}
-      components={{
-        CaptionLabel: (date: CaptionLabelProps) => (
-          <p>
-            {date.displayMonth.getFullYear()} {date.displayMonth.getMonth() + 1}월
-          </p>
-        ),
-        IconLeft: ({ ...props }) => (
-          <ChevronLeftIcon
-            {...props}
-            className="h-5 w-5"
-          />
-        ),
-        IconRight: ({ ...props }) => (
-          <ChevronRightIcon
-            {...props}
-            className="h-5 w-5"
-          />
-        ),
-      }}
-    />
+    <BottomSheet
+      maxHeight={"fit-content"}
+      defaultHeight={"fit-content"}
+      isShow
+    >
+      <DayPicker
+        locale={ko}
+        fixedWeeks
+        mode={"single"}
+        selected={selected}
+        onSelect={setSelected}
+        className="w-full pb-[1.5rem] px-[2.4rem]"
+        classNames={BottomSheetCalenderStyles}
+        components={{
+          CaptionLabel: (date: CaptionLabelProps) => (
+            <p>
+              {date.displayMonth.getFullYear()} {date.displayMonth.getMonth() + 1}월
+            </p>
+          ),
+          IconLeft: ({ ...props }) => (
+            <ChevronLeftIcon
+              {...props}
+              className="h-5 w-5"
+              fill="#8E8E8E"
+            />
+          ),
+          IconRight: ({ ...props }) => (
+            <ChevronRightIcon
+              {...props}
+              className="h-5 w-5"
+              fill="#8E8E8E"
+            />
+          ),
+        }}
+      />
+      <div className="px-[2.4rem]">
+        <button
+          className="w-[100%] max-w-[50rem] h-[5.6rem] bg-example_gray_700 rounded-radius15 mb-[2rem]"
+          onClick={handleSelectedDate}
+        >
+          확인
+        </button>
+      </div>
+    </BottomSheet>
   );
 };
 
--- src/app/_components/client/Calender/Calender.styles.ts
+++ src/app/_components/client/Calender/Calender.styles.ts
@@ -1,15 +1,15 @@
 export const BottomSheetCalenderStyles = {
-  caption: "flex justify-start py-2 mb-4 relative w-full select-none",
+  caption: "flex justify-between py-2 mb-8 relative w-full select-none",
   caption_label: "text-size15 font-medium text-gray-900",
   nav: "flex items-center",
   nav_button:
     "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300 text-example_gray_1200",
   table: "w-full border-collapse",
   head_row: "flex font-medium text-example_gray_1100 select-none",
-  head_cell: "m-0.5 w-full font-normal text-size11",
+  head_cell: "m-0.5 w-full h-[3rem] font-normal text-size11",
   row: "flex w-full mt-2",
-  cell: "rounded-md h-9 w-full text-center text-sm p-0 m-0.5 relative",
-  day: "h-9 w-9 p-0 font-normal",
+  cell: "rounded-md h-[3rem] w-full text-center text-[1.1rem] p-0 m-0.5 relative",
+  day: "h-[3rem] w-[3rem] p-0 font-normal",
   day_range_end: "day-range-end",
   day_selected: "rounded-full bg-example_gray_1000 text-white focus:bg-example_gray_1000",
   day_outside:
