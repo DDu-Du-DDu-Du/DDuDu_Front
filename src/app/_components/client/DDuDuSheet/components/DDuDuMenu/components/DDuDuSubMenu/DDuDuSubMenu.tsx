45b3af64ae9ca8f33edee4ddaaaa8067943ae4d2
seunghyun <tmdgus7820@naver.com>
2024-07-14 18:43:44 +0900

    🛠️refactor: BottomSheet 스타일 수정 및 DDuDuSheet 기능 추가

--- src/app/_components/client/DDuDuSheet/components/DDuDuMenu/components/DDuDuSubMenu/DDuDuSubMenu.tsx
+++ src/app/_components/client/DDuDuSheet/components/DDuDuMenu/components/DDuDuSubMenu/DDuDuSubMenu.tsx
@@ -1,47 +1,76 @@
 import { SheetButton } from "@/app/_components/client";
 import { ExampleIcon } from "@/app/_components/server";
 
-const DDuDuSubMenu = () => {
+interface DDuDuSubMenuProps {
+  isDDuDuDateNow: boolean;
+  status: "COMPLETE" | "UNCOMPLETED";
+  handleSelectDifferentDate: () => void;
+  handleAlarmSetting: () => void;
+}
+
+const DDuDuSubMenu = ({
+  isDDuDuDateNow,
+  status,
+  handleSelectDifferentDate,
+  handleAlarmSetting,
+}: DDuDuSubMenuProps) => {
   return (
     <div className="flex w-full max-w-[50rem] flex-col gap-[1.6rem]">
-      <SheetButton
-        Icon={<ExampleIcon />}
-        title="오늘 또 하기"
-        buttonType="sub"
-        rightPlace={<p>5 일전</p>}
-      />
-      <SheetButton
-        Icon={<ExampleIcon />}
-        title="오늘 다시 하기"
-        buttonType="sub"
-        rightPlace={<p>5 일전</p>}
-      />
-      <SheetButton
-        Icon={<ExampleIcon />}
-        title="다른날 또 하기"
-        buttonType="sub"
-      />
-      <SheetButton
-        Icon={<ExampleIcon />}
-        title="날짜 바꾸기"
-        buttonType="sub"
-      />
-      <SheetButton
-        Icon={<ExampleIcon />}
-        title="알림 설정하기"
-        buttonType="sub"
-        rightPlace={<p>5 일전</p>}
-      />
-      <SheetButton
-        Icon={<ExampleIcon />}
-        title="다른날 반복하기"
-        buttonType="sub"
-      />
-      <SheetButton
-        Icon={<ExampleIcon />}
-        title="미루기"
-        buttonType="sub"
-      />
+      {status === "COMPLETE" && (
+        <>
+          <SheetButton
+            Icon={<ExampleIcon />}
+            title="오늘 또 하기"
+            buttonType="sub"
+            rightPlace={<p>5 일전</p>}
+          />
+          <SheetButton
+            Icon={<ExampleIcon />}
+            title="다른날 또 하기"
+            buttonType="sub"
+            onClick={handleSelectDifferentDate}
+          />
+          <SheetButton
+            Icon={<ExampleIcon />}
+            title="날짜 바꾸기"
+            buttonType="sub"
+            onClick={handleSelectDifferentDate}
+          />
+        </>
+      )}
+      {status === "UNCOMPLETED" && isDDuDuDateNow && (
+        <>
+          <SheetButton
+            Icon={<ExampleIcon />}
+            title="알림 설정하기"
+            buttonType="sub"
+            rightPlace={<p>5 일전</p>}
+            onClick={handleAlarmSetting}
+          />
+          <SheetButton
+            Icon={<ExampleIcon />}
+            title="미루기"
+            buttonType="sub"
+            onClick={handleSelectDifferentDate}
+          />
+        </>
+      )}
+      {status === "UNCOMPLETED" && !isDDuDuDateNow && (
+        <SheetButton
+          Icon={<ExampleIcon />}
+          title="오늘 다시 하기"
+          buttonType="sub"
+          rightPlace={<p>5 일전</p>}
+        />
+      )}
+      {status === "UNCOMPLETED" && (
+        <SheetButton
+          Icon={<ExampleIcon />}
+          title="다른날 반복하기"
+          buttonType="sub"
+          onClick={handleSelectDifferentDate}
+        />
+      )}
     </div>
   );
 };
--- src/app/_components/client/SheetButton/hooks/useSheetButtonStyle/useSheetButtonStyle.ts
+++ src/app/_components/client/SheetButton/hooks/useSheetButtonStyle/useSheetButtonStyle.ts
@@ -15,7 +15,7 @@ const useSheetButtonStyle = ({ buttonType }: UseSheetButtonStyleProps) => {
         "text-size13 flex items-center",
 
         buttonType === "main" &&
-          "h-[8rem] flex-grow-[2] bg-example_gray_100 flex-col justify-center gap-[0.6rem] rounded-radius10",
+          "h-[8rem] flex-grow-[2] bg-white_100 flex-col justify-center gap-[0.6rem] rounded-radius10",
 
         buttonType === "sub" && "w-full h-[3.2rem] gap-[1.6rem]",
       ),
