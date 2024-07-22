25b7e6d8c836998d1a279499e7c42ef686c16c20
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-22 09:49:13 +0900

    🛠️refactor: formatDateRange 마이그레이션

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as scrollToTop } from "./scrollToTop/scrollToTop";
 export { default as formatDateToYYYYMMDD } from "./formatDateToYYYYMMDD/formatDateToYYYYMMDD";
+export { default as formatDateRange } from "./formatDateRange/formatDateRange";
