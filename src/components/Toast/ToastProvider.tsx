d10c670463598474d025f0488a147fbcdf0f935b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-26 18:04:47 +0900

    💎feat: ToastStore 적용 및 ToastItem 생성

--- src/components/Toast/ToastProvider.tsx
+++ src/components/Toast/ToastProvider.tsx
@@ -1,15 +1,30 @@
 import * as S from "./ToastProvider.styles";
 
+import { ToastItem } from "./components";
+import { useToastStore } from "./store";
+
 export interface ToastProviderProps {
   children: React.ReactNode;
 }
 
 const ToastProvider = ({ children }: ToastProviderProps) => {
+  const { toastList, removeToast } = useToastStore();
+
   return (
     <>
       {children}
 
-      <S.ToastBox>ToastProvider</S.ToastBox>
+      <S.ToastBox>
+        {toastList.map(({ id, message, deleteTime, type }) => (
+          <ToastItem
+            key={id}
+            message={message}
+            deleteTime={deleteTime}
+            type={type}
+            onRemove={() => removeToast(id)}
+          />
+        ))}
+      </S.ToastBox>
     </>
   );
 };
