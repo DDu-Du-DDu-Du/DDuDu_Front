8822db707ec38429f9472f91960fa233400fac4a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-26 20:19:13 +0900

    🐛fix: Storybook webpack path alias 미인식 문제 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -14,6 +16,20 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
+
+  webpackFinal: async (prevConfig) => {
+    const newConfig = { ...prevConfig };
+
+    if (newConfig.resolve) {
+      newConfig.resolve.alias = {
+        ...newConfig.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+
+    return newConfig;
+  },
+
   staticDirs: ["../public"],
 };
 export default config;
