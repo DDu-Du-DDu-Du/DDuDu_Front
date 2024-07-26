55f1580de6f907749ff72396eec262e3c2f25d4d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-26 20:11:03 +0900

    💎feat: hexConvertForRGBA 유틸 생성

---
+++ src/lib/utils/hexConvertForRGBA/hexConvertForRGBA.ts
@@ -0,0 +1,18 @@
+interface HexConvertForRGBAparams {
+  hex: string;
+  alpha: number;
+}
+
+const hexConvertForRGBA = ({ hex, alpha }: HexConvertForRGBAparams) => {
+  let HEXcode = hex;
+
+  if (HEXcode.startsWith("#")) {
+    HEXcode = hex.replace(/#/g, "");
+  }
+
+  const rgbList = HEXcode.match(/.{2}/g)?.map((replacer) => parseInt(replacer, 16) || 0) || [0];
+
+  return `rgba(${rgbList.join(",")},${alpha})`;
+};
+
+export default hexConvertForRGBA;
