805479658f51ca60fd7d548168b7caf001af2bf6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-27 17:48:16 +0900

    💎feat: TextInput 컴포넌트 생성

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from "./icons";
 
 export { default as ToastProvider } from "./Toast/ToastProvider";
 export { default as Timeline } from "./Timeline/Timeline";
+export { default as TextInput } from "./TextInput/TextInput";
 
 // hooks
 export * from "./Toast/hooks";
