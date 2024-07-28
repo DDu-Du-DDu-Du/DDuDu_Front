c1194de98777c4abe5e8bd26cfd32eb1f924559d
seunghyun <tmdgus7820@naver.com>
2024-07-28 21:44:54 +0900

    💎feat: InputTime 컴포넌트 제작

---
+++ src/app/_components/client/InputTime/InputTime.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import styles from "./InputTime.module.css";
+
+import { ChangeEventHandler, useState } from "react";
+import { useFormContext } from "react-hook-form";
+
+interface InputTimeProps {
+  id: string;
+  label: string;
+  name: string;
+  handleTimeChange: (time: string) => void;
+}
+
+const InputTime = ({ id, name, handleTimeChange, label }: InputTimeProps) => {
+  const [time, setTime] = useState(label);
+  const { register } = useFormContext();
+
+  const handleInputTimeChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTime(event.target.value);
+    handleTimeChange(event?.target.value);
+  };
+
+  return (
+    <div className={`${styles.timePicker} relative w-[12rem]`}>
+      <div className="h-[4rem] rounded-radius10 cursor-pointer leading-[4rem] pl-[1.8rem] bg-example_gray_100">
+        <label
+          htmlFor={name}
+          className="text-size13"
+        >
+          {time}
+        </label>
+        <input
+          className="absolute top-0 left-0 w-full h-full opacity-0"
+          type="time"
+          id={id}
+          {...register(name, {
+            required: true,
+          })}
+          onChange={handleInputTimeChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default InputTime;
