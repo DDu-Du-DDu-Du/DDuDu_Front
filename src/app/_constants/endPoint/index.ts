d2d782476c7ab62426f428c66bf3ac33a3d1c0ac
SeungHyune <tmdgus7820@naver.com>
2024-08-12 11:53:06 +0900

    💎feat: goalEditor api 호출 함수 추가

--- src/app/_constants/endPoint/index.ts
+++ src/app/_constants/endPoint/index.ts
@@ -2,3 +2,4 @@ export { default as AUTH } from "./auth/auth";
 export { default as USER } from "./user/user";
 export { default as GOAL } from "./goal/goal";
 export { default as FEED } from "./feed/feed";
+export { default as GOAL_EDITOR } from "./goalEditor/goalEditor";
