2c5c4643bfc65e35a8856eca44b250fdb07f30b5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-16 15:02:18 +0900

    💎feat: useClickAway hook 생성

---
+++ src/lib/hooks/useClickAway/useClickAway.ts
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+
+type CallbackType = (event?: MouseEvent | TouchEvent) => void;
+
+const useClickAway = <T>(callback: CallbackType) => {
+  const ref = useRef<T | null>(null);
+  const memoCallback = useRef(callback);
+
+  useEffect(() => {
+    memoCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const { current } = ref;
+
+    if (!current || !(current instanceof HTMLElement)) {
+      return;
+    }
+
+    const eventHandler = (event: MouseEvent | TouchEvent) => {
+      const { target } = event;
+
+      if (!target || !(target instanceof HTMLElement)) {
+        return;
+      }
+
+      if (current.contains(target)) {
+        return;
+      }
+
+      memoCallback.current(event);
+    };
+
+    window.addEventListener("mousedown", eventHandler);
+    window.addEventListener("touchstart", eventHandler);
+
+    return () => {
+      window.removeEventListener("mousedown", eventHandler);
+      window.removeEventListener("touchstart", eventHandler);
+    };
+  }, []);
+
+  return ref;
+};
+
+export default useClickAway;
