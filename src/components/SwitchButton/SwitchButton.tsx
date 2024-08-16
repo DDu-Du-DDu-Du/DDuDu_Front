95afd1a4fa46f59cd4711d62acce1cf6515858f6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-16 13:59:12 +0900

    💎feat: SwitchButton Type 지정

--- src/components/SwitchButton/SwitchButton.tsx
+++ src/components/SwitchButton/SwitchButton.tsx
@@ -1,20 +1,34 @@
 import * as S from "./SwitchButton.styles";
 
-const SwitchButton = () => {
+interface SwitchButtonProps {
+  firstLabel?: string;
+  secondLabel?: string;
+  // viewKey?: string;
+  // selectedOption?: string;
+  // alternativeOption?: string;
+}
+
+const SwitchButton = ({
+  firstLabel = "투두",
+  secondLabel = "스케줄",
+  // viewKey = "view",
+  // selectedOption = "ddudu",
+  // alternativeOption = "schedule",
+}: SwitchButtonProps) => {
   return (
     <S.SwitchButtonLayout>
       <S.SwitchButtonLabel
         className="relative flex justify-center items-center w-[50%] box-border z-10"
         // onClick={handleToggleToFirst}
       >
-        {/* {firstLabel} */}
+        {firstLabel}
       </S.SwitchButtonLabel>
 
       <S.SwitchButtonLabel
         className="relative flex justify-center items-center w-[50%] box-border z-10"
         // onClick={handleToggleToSecond}
       >
-        {/* {secondLabel} */}
+        {secondLabel}
       </S.SwitchButtonLabel>
       <S.SwitchButtonMove $isToggle={false} />
     </S.SwitchButtonLayout>
