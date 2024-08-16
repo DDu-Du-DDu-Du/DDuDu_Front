0d2ed999756022add2868cee385b545a03eebc93
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-16 14:45:02 +0900

    💎feat: MonthlyGoal 컴포넌트 생성

---
+++ src/components/MonthlyGoal/MonthlyGoal.tsx
@@ -0,0 +1,7 @@
+import * as S from "./MonthlyGoal.styles";
+
+const MonthlyGoal = () => {
+  return <S.MonthlyGoalLayout>MonthlyGoal</S.MonthlyGoalLayout>;
+};
+
+export default MonthlyGoal;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export { default as ToastProvider } from "./Toast/ToastProvider";
 export { default as Timeline } from "./Timeline/Timeline";
 export { default as TextInput } from "./TextInput/TextInput";
 export { default as SheetButton } from "./SheetButton/SheetButton";
+export { default as MonthlyGoal } from "./MonthlyGoal/MonthlyGoal";
 
 // hooks
 export * from "./Toast/hooks";
