ca0b9f551aadbb9b7ffd95f7031ba2562fec6eae
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-08-22 21:55:00 +0900

    🎨style: Modal background 추가

--- src/components/Modal/Modal.styles.tsx
+++ src/components/Modal/Modal.styles.tsx
@@ -2,33 +2,34 @@ import styled from "styled-components";
 
 import { motion } from "framer-motion";
 
-export const ModalBackground = styled(motion.section)<{
+export const ModalBackground = styled(motion.section)`
+  width: 100%;
+  height: 100%;
+
+  position: fixed;
+  top: 0;
+  left: 0%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #23232390;
+
+  z-index: ${({ theme }) => theme.zIndex.modal};
+`;
+
+export const ModalLayout = styled(motion.article)<{
   $width: string;
   $height: string;
   $backgroundColor?: string;
 }>`
   width: ${({ $width }) => $width};
   height: ${({ $height }) => $height};
-  position: fixed;
-  left: 50%;
-  top: 50%;
+
+  position: relative;
 
   border-radius: ${({ theme }) => theme.borderRadius.radius10};
   background-color: ${({ $backgroundColor }) => $backgroundColor};
   box-shadow: ${({ theme }) => theme.boxShadow.shadow_100};
-
-  text-align: center;
-
-  z-index: ${({ theme }) => theme.zIndex.modal};
-`;
-
-export const ModalSpacing = styled.div`
-  width: 100%;
-  height: 100%;
-
-  position: fixed;
-  top: 10rem;
-
-  opacity: 0;
-  z-index: ${({ theme }) => theme.zIndex.modal};
 `;
