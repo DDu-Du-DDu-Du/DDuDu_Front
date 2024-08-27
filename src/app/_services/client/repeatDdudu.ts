c46ebb88ed6e37071949080118d1f0a703e78c93
seunghyun <tmdgus7820@naver.com>
2024-08-27 16:09:19 +0900

    💎feat: fetchEditRepeatDDudu API 함수 추가 - repeatDdudu API 함수 타입 재정의

--- src/app/_services/client/repeatDdudu.ts
+++ src/app/_services/client/repeatDdudu.ts
@@ -1,9 +1,10 @@
 import { fetchApi } from "@/app/_api/";
 import { REPEAT_DDUDU } from "@/app/_constants";
+import { RepeatDduduRequestType } from "@/app/_types/request/repeatDdudu/repeatDdudu";
 
 interface FetchCreateRepeatDduduProps {
   accessToken: string;
-  repeatDDuDuData: unknown;
+  repeatDDuDuData: RepeatDduduRequestType & { goalId: string };
 }
 
 export const fetchCreateRepeatDDudu = async ({
@@ -19,6 +20,36 @@ export const fetchCreateRepeatDDudu = async ({
     },
     next: {
       tags: ["repeat", "ddudu"],
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface fetchEditRepeatDDuduProps {
+  accessToken: string;
+  repeatDDuDuData: RepeatDduduRequestType;
+  repeatId: string;
+}
+
+export const fetchEditRepeatDDudu = async ({
+  accessToken,
+  repeatDDuDuData,
+  repeatId,
+}: fetchEditRepeatDDuduProps) => {
+  const response = await fetchApi(`${REPEAT_DDUDU.CREATE}`, {
+    method: "PUT",
+    body: JSON.stringify(repeatDDuDuData),
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    next: {
+      tags: ["repeat", "ddudu", repeatId],
     },
   });
 
