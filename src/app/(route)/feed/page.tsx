30ce853c1ad042585e1c18a78572567685371459
seunghyun <tmdgus7820@naver.com>
2024-08-30 15:14:33 +0900

    🗑️remove: feed 페이지 prefetch 기능 제거 - 자주 페이지 전환 API 콜이 필요한 페이지의 경우 prefetch를 활용하는 것이 효율적이지 않다고 판단

--- src/app/(route)/feed/page.tsx
+++ src/app/(route)/feed/page.tsx
@@ -1,8 +1,4 @@
-// import { MonthlyGoalsType } from "@/app/_components/client/Calender/FeedCalender/FeedCalender";
-import { getDailyList } from "@/app/_services/client";
 import { formatDateToYYYYMMDD } from "@/app/_utils";
-import { auth } from "@/auth";
-import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 
 import { MainFeed, MainHeader, MainSchedule } from "./components";
 import { TIMETABLE_DDUDU_LIST } from "./feed.constants";
@@ -14,47 +10,20 @@ interface FeedPageProps {
 const FeedPage = async ({ searchParams }: FeedPageProps) => {
   const { view, date = formatDateToYYYYMMDD(new Date()) } = searchParams;
 
-  const session = await auth();
-
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["dailyList", date],
-    queryFn: () =>
-      getDailyList({
-        accessToken: session?.sessionToken as string,
-        userId: session?.user.userId as number,
-        date,
-      }),
-  });
-
-  const dehydratedState = dehydrate(queryClient);
-
-  // const dailyTimeTable: MainDailyTimeTableType[] = await getDailyTimeTable(
-  //   session?.sessionToken as string,
-  //   session?.user.userId as number,
-  // );
-
-  // const monthlyGoals: MonthlyGoalsType[] = await getMonthlyGoals({
-  //   accessToken: session?.sessionToken as string,
-  //   type: view === "ddudu" ? "MONTH" : "WEEK",
-  // });
-
   return (
-    <HydrationBoundary state={dehydratedState}>
-      <div className="py-[2.4rem] px-[2.4rem]">
-        <MainHeader />
-        {view === "ddudu" && (
-          <MainFeed
-            monthlyGoals={[
-              { id: 0, contents: "테스트 1", type: "WEEK" },
-              { id: 1, contents: "테스트 2", type: "WEEK" },
-            ]}
-            selectedDDuDu={date}
-          />
-        )}
-        {view === "schedule" && <MainSchedule dailyTimeTable={TIMETABLE_DDUDU_LIST} />}
-      </div>
-    </HydrationBoundary>
+    <div className="py-[2.4rem] px-[2.4rem]">
+      <MainHeader />
+      {view === "ddudu" && (
+        <MainFeed
+          monthlyGoals={[
+            { id: 0, contents: "테스트 1", type: "WEEK" },
+            { id: 1, contents: "테스트 2", type: "WEEK" },
+          ]}
+          selectedDDuDu={date}
+        />
+      )}
+      {view === "schedule" && <MainSchedule dailyTimeTable={TIMETABLE_DDUDU_LIST} />}
+    </div>
   );
 };
 
