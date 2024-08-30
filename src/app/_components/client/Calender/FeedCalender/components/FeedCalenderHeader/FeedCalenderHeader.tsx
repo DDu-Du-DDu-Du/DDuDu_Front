7afd85e1d98290fdf8e3a7f89fabf5e47e32e4cc
seunghyun <tmdgus7820@naver.com>
2024-08-30 15:19:21 +0900

    💎feat: prevMonth, nextMonth 버튼을 통해 이전, 다음 달 조회 가능하도록 기능 추가 - 버튼을 통해 이동한 연도-월을 변환해 주는 유틸 함수 추가 convertCurrentDate - 기존 useQuery로 데이터 패칭한 데이터를 setQueryData의 queryKey를 통해 데이터에 접근하여 데이터 업데이트

--- src/app/_components/client/Calender/FeedCalender/components/FeedCalenderHeader/FeedCalenderHeader.tsx
+++ src/app/_components/client/Calender/FeedCalender/components/FeedCalenderHeader/FeedCalenderHeader.tsx
@@ -5,16 +5,64 @@ import { CaptionProps, useNavigation } from "react-day-picker";
 import ChevronLeftIcon from "@/app/_components/server/icons/ChevronLeftIcon/ChevronLeftIcon";
 import ChevronRightIcon from "@/app/_components/server/icons/ChevronRightIcon/ChevronRightIcon";
 import { useToggle } from "@/app/_hooks";
+import { getMonthlyDDuDus } from "@/app/_services/client";
+import { MonthlyDDuDuType } from "@/app/_types/response/feed/feed";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { MonthlyGoalsType } from "../../FeedCalender";
 import { MainGoalEditModal } from "./components";
+
+import { useSession } from "next-auth/react";
 
 const FeedCalenderHeader = (props: CaptionProps, monthlyGoal?: MonthlyGoalsType[]) => {
   const { goToMonth, nextMonth, previousMonth } = useNavigation();
   const currentYear = props.displayMonth.getFullYear();
   const currentMonth = props.displayMonth.getMonth() + 1;
 
+  const convertCurrentDate = (type: "next" | "prev") => {
+    let year = currentYear;
+    let month = currentMonth;
+
+    if (type === "next") {
+      year = currentMonth + 1 > 12 ? currentYear + 1 : currentYear;
+      month = currentMonth + 1 > 12 ? 1 : currentMonth + 1;
+    } else if (type === "prev") {
+      year = currentMonth - 1 < 1 ? currentYear - 1 : currentYear;
+      month = currentMonth - 1 < 1 ? 12 : currentMonth - 1;
+    }
+
+    return { year, month };
+  };
+
+  const { data: session } = useSession();
+  const queryClient = useQueryClient();
   const { isToggle, handleToggleOff, handleToggleOn } = useToggle();
+
+  const MonthlyDDuDUs = async (type: "next" | "prev") => {
+    const { year, month } = convertCurrentDate(type);
+    const currentYYYYMM = `${year}-${month < 10 ? "0" : ""}${month}`;
+    return await queryClient.fetchQuery<MonthlyDDuDuType[]>({
+      queryKey: ["monthlyDDuDus", year, month],
+      queryFn: () =>
+        getMonthlyDDuDus({
+          accessToken: session?.sessionToken as string,
+          userId: session?.user.userId as number,
+          date: currentYYYYMM,
+        }),
+    });
+  };
+
+  const handleNextToMonth = async () => {
+    nextMonth && goToMonth(nextMonth);
+    const newMonthlyDDuDus = await MonthlyDDuDUs("next");
+    queryClient.setQueryData(["monthlyDDuDus"], () => newMonthlyDDuDus);
+  };
+
+  const handlePrevToMonth = async () => {
+    previousMonth && goToMonth(previousMonth);
+    const newMonthlyDDuDus = await MonthlyDDuDUs("prev");
+    queryClient.setQueryData(["monthlyDDuDus"], () => newMonthlyDDuDus);
+  };
 
   return (
     <header className="flex flex-col items-center px-4 py-2">
@@ -24,13 +72,13 @@ const FeedCalenderHeader = (props: CaptionProps, monthlyGoal?: MonthlyGoalsType[
         </header>
         <article className="flex items-center">
           <button
-            onClick={() => previousMonth && goToMonth(previousMonth)}
+            onClick={handlePrevToMonth}
             className="w-auto py-[0.2rem] px-[0.5rem]"
           >
             <ChevronLeftIcon className="h-5 w-5 fill-example_gray_1200" />
           </button>
           <button
-            onClick={() => nextMonth && goToMonth(nextMonth)}
+            onClick={handleNextToMonth}
             className="w-auto py-[0.2rem] px-[0.5rem]"
           >
             <ChevronRightIcon className="h-5 w-5 fill-example_gray_1200" />
