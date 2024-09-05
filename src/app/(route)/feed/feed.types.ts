8696fe6d070b24a7caa4a2d94fa901d8b65d1f88
seunghyun <tmdgus7820@naver.com>
2024-09-05 12:27:14 +0900

    🚜change: feed.types에 정의한 types 중 feed API response에 대한 type 위치 변경 - feed.types.ts에서 response/feed/feed.ts로 types 정의 위치 변경 - feed.types를 참조하는 타입들 파일들도 모두 변경

--- src/app/(route)/feed/feed.types.ts
+++ src/app/(route)/feed/feed.types.ts
@@ -1,35 +1,3 @@
-export type MainGoalType = {
-  id: number;
-  name: string;
-  color: string;
-};
-
-export type MainDDuDusType = {
-  id: number;
-  name: string;
-  status: "COMPLETE" | "UNCOMPLETED";
-};
-
-export interface MainDailyListType {
-  goal: MainGoalType;
-  ddudus: MainDDuDusType[];
-}
-
-export interface MainDailyTimeTableType {
-  timetable: MainTimeTableType[];
-  unassignedDdudus: MainDailyListType[];
-}
-
-export type MainTimeTableType = {
-  beginAt: string;
-  ddudus: {
-    id: number;
-    name: string;
-    status: "COMPLETE" | "UNCOMPLETED";
-    goalId: number;
-  }[];
-};
-
 export interface DDuDuTimeType {
   beginAt: string;
   endAt: string;
--- src/app/_types/response/feed/feed.ts
+++ src/app/_types/response/feed/feed.ts
@@ -1,5 +1,41 @@
+type MainGoalType = {
+  id: number;
+  name: string;
+  color: string;
+};
+
+export type MainDDuDusType = {
+  id: number;
+  name: string;
+  status: "COMPLETE" | "UNCOMPLETED";
+};
+
+export interface MainDailyListType {
+  goal: MainGoalType;
+  ddudus: MainDDuDusType[];
+}
+
 export interface MonthlyDDuDuType {
   date: string;
   totalCount: number;
   uncompletedCount: number;
+}
+
+export interface MainDailyTimeTableType {
+  timetable: MainTimeTableType[];
+  unassignedDdudus: MainDailyListType[];
+}
+
+export type MainTimeTableType = {
+  beginAt: string;
+  ddudus: MainTimeTableDDuDuType[];
+};
+
+interface MainTimeTableDDuDuType {
+  id: number;
+  name: string;
+  status: "COMPLETE" | "UNCOMPLETED";
+  goalId: number;
+  beginAt?: string;
+  endAt?: string;
 }
