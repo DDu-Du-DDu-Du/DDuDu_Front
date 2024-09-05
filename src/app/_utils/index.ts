4f80a8826cdd4d274cb0259f6da5358583f89e5a
seunghyun <tmdgus7820@naver.com>
2024-09-05 11:52:22 +0900

    🏷️rename: utils 함수 import 방식 수정 - formatDateToYYYYMMDD를 사용하는 파일들의 import 문도 함께 수정

--- src/app/_utils/index.ts
+++ src/app/_utils/index.ts
@@ -1,2 +1,3 @@
 export { default as scrollToTop } from "./scrollToTop/scrollToTop";
 export { default as formatDateRange } from "./formatDateRange/formatDateRange";
+export { default as formatDateToYYYYMMDD } from "./formatDateToYYYYMMDD/formatDateToYYYYMMDD";
