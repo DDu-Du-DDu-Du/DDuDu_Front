a95c6d886f8ddd776bc3a68abc60990ea6866fba
seunghyun <tmdgus7820@naver.com>
2024-09-05 18:09:47 +0900

    💎feat: useDDuDuDateMutation hook 생성 - DDuDuDate에 대한 Mutation 기능을 hook으로 분리 - 투두리스트 날짜 변경 기능, 현재 투두리스트를 다른 날짜에도 반복하는 기능

---
+++ src/app/(route)/feed/components/MainGoalItem/hooks/useDDuDuDateMutation/useDDuDuDateMutation.tsx
@@ -0,0 +1,83 @@
+import { FEED_KEY } from "@/app/_constants/queryKey/queryKey";
+import { fetchDDuDuChangeDate, fetchDDuDuRepeatDate } from "@/app/_services/client";
+import { formatDateToYYYYMMDD } from "@/app/_utils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+interface UseDDuDuDateMutationProps {
+  sessionToken: string;
+  currentDDuDuId: number;
+  currentCalendarType: "change" | "repeat";
+  handleSelectedDate: (selectedDate: Date | undefined) => void;
+  handleCalendarSheetToggleOff: () => void;
+  handleDDuDuSheetToggleOff: () => void;
+}
+
+const useDDuDuDateMutation = ({
+  sessionToken,
+  currentDDuDuId,
+  currentCalendarType,
+  handleSelectedDate,
+  handleCalendarSheetToggleOff,
+  handleDDuDuSheetToggleOff,
+}: UseDDuDuDateMutationProps) => {
+  const queryClient = useQueryClient();
+
+  const handleSuccessDate = () => {
+    queryClient.invalidateQueries({
+      queryKey: [FEED_KEY.DAILY_LIST],
+    });
+    queryClient.refetchQueries({
+      queryKey: [FEED_KEY.DAILY_LIST],
+    });
+    queryClient.refetchQueries({ queryKey: [FEED_KEY.MONTHLY_DDUDUS] });
+
+    handleSelectedDate(undefined);
+    handleCalendarSheetToggleOff();
+  };
+
+  const dduduChangeDateMutation = useMutation({
+    mutationKey: [FEED_KEY.DDUDU_CHANGE_DATE],
+    mutationFn: fetchDDuDuChangeDate,
+    onSuccess: handleSuccessDate,
+  });
+
+  const dduduRepeatDateMutation = useMutation({
+    mutationKey: [FEED_KEY.DDUDU_REPEAT_DATE],
+    mutationFn: fetchDDuDuRepeatDate,
+    onSuccess: () => {
+      handleSuccessDate();
+      handleDDuDuSheetToggleOff();
+    },
+  });
+
+  const onChangeDDuDuDate = (selectedDate: Date) => {
+    if (currentCalendarType === "change") {
+      dduduChangeDateMutation.mutate({
+        accessToken: sessionToken,
+        id: currentDDuDuId,
+        date: formatDateToYYYYMMDD(selectedDate),
+      });
+    } else if (currentCalendarType === "repeat") {
+      dduduRepeatDateMutation.mutate({
+        accessToken: sessionToken,
+        id: currentDDuDuId,
+        date: formatDateToYYYYMMDD(selectedDate),
+      });
+    }
+  };
+
+  const onRepeatCurrentDate = () => {
+    dduduRepeatDateMutation.mutate({
+      accessToken: sessionToken,
+      id: currentDDuDuId,
+      date: formatDateToYYYYMMDD(new Date()),
+    });
+  };
+
+  return {
+    onChangeDDuDuDate,
+    onRepeatCurrentDate,
+  };
+};
+
+export default useDDuDuDateMutation;
