da1901e99542c047e3e6f8cf1f681d92240f60b3
seunghyun <tmdgus7820@naver.com>
2024-09-06 17:48:21 +0900

    🛠️refactor: PrivacySheet 컴포넌트 수정 - 기존 버튼을 누르면 즉시 privacyType이 선택되며 상태가 변경되었지만, 리스트를 라디오 형태로 바꿈으로써 privacyType을 선택한 후 확인 버튼을 누르지 않으면 상태가 변경되지 않도록 처리했습니다. - 라디오 버튼을 통해 현재 선택한 리스트가 어떤 것인지 명시적으로 나타내주었습니다.

--- src/app/_components/client/PrivacySheet/PrivacySheet.tsx
+++ src/app/_components/client/PrivacySheet/PrivacySheet.tsx
@@ -1,24 +1,41 @@
-import { BottomSheet, Button, SheetButton } from "@/app/_components/client";
-import { ExampleIcon } from "@/app/_components/server";
+"use client";
+
+import { Fragment } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+
+import { BottomSheet, Button } from "@/app/_components/client";
 import { GoalPrivacyType } from "@/app/_types/response/goal/goal";
 
+import { SheetRadioItem } from "./components";
+import { PRIVACY_LIST } from "./privacySheet.constants";
+
+interface privacyFormType {
+  privacyType: GoalPrivacyType;
+}
+
 interface PrivacySheetProps {
+  goalPrivacy: GoalPrivacyType;
   isShow: boolean;
   onClose: () => void;
   onClick: (goalPrivacy: GoalPrivacyType) => void;
 }
 
-const PrivacySheet = ({ isShow, onClose, onClick }: PrivacySheetProps) => {
-  const handleClickPublic = () => {
-    onClick("PUBLIC");
+const PrivacySheet = ({ goalPrivacy, isShow, onClose, onClick }: PrivacySheetProps) => {
+  const methods = useForm<privacyFormType>({
+    defaultValues: {
+      privacyType: goalPrivacy,
+    },
+  });
+
+  const onValid: SubmitHandler<privacyFormType> = ({ privacyType }) => {
+    onClick(privacyType);
+    onClose();
   };
 
-  const handleClickFollower = () => {
-    onClick("FOLLOWER");
-  };
-
-  const handleClickPrivate = () => {
-    onClick("PRIVATE");
+  const handlePrivacySubmit = (event: React.FormEvent) => {
+    event.stopPropagation();
+    event.preventDefault();
+    methods.handleSubmit(onValid)(event);
   };
 
   return (
@@ -28,36 +45,33 @@ const PrivacySheet = ({ isShow, onClose, onClick }: PrivacySheetProps) => {
       defaultHeight="fit-content"
       maxHeight="fit-content"
     >
-      <div className="p-[2rem] box-border">
-        <div className="flex flex-col px-[0.4rem] gap-[1.6rem] mb-[2.1rem]">
-          <SheetButton
-            buttonType="sub"
-            title="전체공개"
-            Icon={<ExampleIcon />}
-            onClick={handleClickPublic}
-          />
-          <SheetButton
-            buttonType="sub"
-            title="팔로워 공개"
-            Icon={<ExampleIcon />}
-            onClick={handleClickFollower}
-          />
-          <SheetButton
-            buttonType="sub"
-            title="나만보기"
-            Icon={<ExampleIcon />}
-            onClick={handleClickPrivate}
-          />
-        </div>
-        <Button
-          onClick={onClose}
-          className="w-full h-[5.6rem]"
-          fontSize="large"
-          backgroundColor="orange"
+      <FormProvider {...methods}>
+        <form
+          className="p-[2rem] box-border"
+          onSubmit={handlePrivacySubmit}
         >
-          확인
-        </Button>
-      </div>
+          <ul className="flex flex-col px-[0.4rem] gap-[1.6rem] mb-[2.1rem]">
+            {PRIVACY_LIST.map(({ id, name, value, label }, index) => (
+              <Fragment key={index}>
+                <SheetRadioItem
+                  id={id}
+                  name={name}
+                  value={value}
+                  label={label}
+                />
+              </Fragment>
+            ))}
+          </ul>
+          <Button
+            className="w-full h-[5.6rem]"
+            type="submit"
+            fontSize="large"
+            backgroundColor="orange"
+          >
+            확인
+          </Button>
+        </form>
+      </FormProvider>
     </BottomSheet>
   );
 };
---
+++ src/app/_components/client/PrivacySheet/privacySheet.types.ts
@@ -0,0 +1,8 @@
+import { GoalPrivacyType } from "@/app/_types/response/goal/goal";
+
+export interface PrivacyItemType {
+  id: "public" | "follower" | "private";
+  label: "전체공개" | "팔로워 공개" | "나만보기";
+  name: "privacyType";
+  value: GoalPrivacyType;
+}
