35c31379c3e0bce65238f15f2b73bb4b292b27c9
seunghyun <tmdgus7820@naver.com>
2024-09-07 12:58:47 +0900

    💎feat: Goal Delete 기능 로직을 useDeleteGoalMutation hook으로 분리

---
+++ src/app/(route)/goal/editor/components/GoalEditor/components/GoalEditorForm/hooks/useDeleteGoalMutation/useDeleteGoalMutation.ts
@@ -0,0 +1,34 @@
+import { GOAL_KEY } from "@/app/_constants/queryKey/queryKey";
+import { fetchDeleteGoal } from "@/app/_services/client/goalEditor";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { useRouter } from "next/navigation";
+
+interface UseDeleteGoalProps {
+  sessionToken: string;
+  goalId: string;
+  reset: () => void;
+}
+
+const useDeleteGoal = ({ sessionToken, goalId, reset }: UseDeleteGoalProps) => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const deleteGoalMutation = useMutation({
+    mutationKey: [GOAL_KEY.GOAL_DELETE, goalId],
+    mutationFn: fetchDeleteGoal,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [GOAL_KEY.GOAL_LIST] });
+      reset();
+      router.replace("/goal");
+    },
+  });
+
+  const handleGoalDelete = () => {
+    deleteGoalMutation.mutate({ accessToken: sessionToken, goalId });
+  };
+
+  return { handleGoalDelete };
+};
+
+export default useDeleteGoal;
