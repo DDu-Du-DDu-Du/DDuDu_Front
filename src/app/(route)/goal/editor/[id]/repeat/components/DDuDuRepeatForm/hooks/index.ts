2f794aae06e9a1216807af8024fe482abdd71e8a
seunghyun <tmdgus7820@naver.com>
2024-09-08 01:54:35 +0900

    💎feat: DDuDuRepeatForm 기능 분리 - DDuDuRepeatForm에 대한 초기 defaultValue 설정 - 작성 중인 목표 작성 페이지로 돌아갔을 때 기존 데이터를 호출할 수 있는 상태 값 저장 - DDuDuRepeatForm의 각 input 필드의 실시간 데이터 저장

---
+++ src/app/(route)/goal/editor/[id]/repeat/components/DDuDuRepeatForm/hooks/index.ts
@@ -0,0 +1,3 @@
+export { default as useRepeatDDuDuMutation } from "./useRepeatDDuDuMutation/useRepeatDDuDuMutation";
+export { default as useDeleteRepeatDDuDuMutation } from "./useDeleteRepeatDDuDuMutation/useDeleteRepeatDDuDuMutation";
+export { default as useRepeatDDuDuEditor } from "./useRepeatDDuDuEditor/useRepeatDDuDuEditor";
