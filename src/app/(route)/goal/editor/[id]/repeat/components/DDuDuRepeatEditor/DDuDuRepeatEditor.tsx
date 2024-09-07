568b23b6f1f08a07e09462d4e0d6cdc8fe65a4c5
seunghyun <tmdgus7820@naver.com>
2024-09-07 23:41:36 +0900

    💎feat: DDuDuRepeatEditor의 로직 hook으로 분리 - 현재 repeatDDuDu 찾기, 현재 repeatDDuDu의 repeatType이 Monthly인 경우 찾아서 number value를 string value로 변경

--- src/app/(route)/goal/editor/[id]/repeat/components/DDuDuRepeatEditor/DDuDuRepeatEditor.tsx
+++ src/app/(route)/goal/editor/[id]/repeat/components/DDuDuRepeatEditor/DDuDuRepeatEditor.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import { useMemo } from "react";
-
-import { useGoalFormStore } from "@/app/_store";
-import { RepeatDdudusType } from "@/app/_types/response/goal/goal";
-
 import DDuDuRepeatForm from "../DDuDuRepeatForm/DDuDuRepeatForm";
-import { DayOfMonthString } from "../DDuDuRepeatForm/DDuDuRepeatForm.types";
+import useRepeatEditor from "./hooks/useRepeatEditor/useRepeatEditor";
 
 interface DDuDuRepeatEditorProps {
   goalId: string;
@@ -14,20 +9,7 @@ interface DDuDuRepeatEditorProps {
 }
 
 const DDuDuRepeatEditor = ({ goalId, repeatId }: DDuDuRepeatEditorProps) => {
-  // goalId가 create라는 것은 처음 생성하는 목표이며, 만약 goalId가 create가 아니라면 이미 만드어진 목표를 수정하는 것임.
-  // 이미 만들어진 목표를 수정할 경우 해당 목표를 useQuery로 불러와야함
-  // repeatId가 있다는 것은 데이터가 있다는 거고 수정이라는 뜻임
-
-  const { repeatDDuDu } = useGoalFormStore();
-
-  const currentRepeatDDuDu: RepeatDdudusType = useMemo(() => {
-    return repeatDDuDu.filter((ddudu) => String(ddudu.id) === repeatId)[0];
-  }, [repeatDDuDu, repeatId]);
-
-  const currentRepeatMonthData =
-    currentRepeatDDuDu && currentRepeatDDuDu.repeatPattern.repeatType === "MONTHLY"
-      ? (currentRepeatDDuDu.repeatPattern.repeatDaysOfMonth?.map(String) as DayOfMonthString[])
-      : [];
+  const { currentRepeatDDuDu, currentRepeatMonthData } = useRepeatEditor({ repeatId });
 
   if (repeatId && !currentRepeatDDuDu) {
     return null;
---
+++ src/app/(route)/goal/editor/[id]/repeat/components/DDuDuRepeatEditor/hooks/useRepeatEditor/useRepeatEditor.ts
@@ -0,0 +1,30 @@
+import { useMemo } from "react";
+
+import { useGoalFormStore } from "@/app/_store";
+import { RepeatDdudusType } from "@/app/_types/response/goal/goal";
+
+import { DayOfMonthString } from "../../../DDuDuRepeatForm/DDuDuRepeatForm.types";
+
+interface UseRepeatEditorProps {
+  repeatId: string;
+}
+
+const useRepeatEditor = ({ repeatId }: UseRepeatEditorProps) => {
+  const { repeatDDuDu } = useGoalFormStore();
+
+  const currentRepeatDDuDu: RepeatDdudusType = useMemo(() => {
+    return repeatDDuDu.filter((ddudu) => String(ddudu.id) === repeatId)[0];
+  }, [repeatDDuDu, repeatId]);
+
+  const currentRepeatMonthData =
+    currentRepeatDDuDu && currentRepeatDDuDu.repeatPattern.repeatType === "MONTHLY"
+      ? (currentRepeatDDuDu.repeatPattern.repeatDaysOfMonth?.map(String) as DayOfMonthString[])
+      : [];
+
+  return {
+    currentRepeatDDuDu,
+    currentRepeatMonthData,
+  };
+};
+
+export default useRepeatEditor;
