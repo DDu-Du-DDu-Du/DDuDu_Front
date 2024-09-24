39436794664a47c6543d7931e7de856af67fc157
seunghyun <tmdgus7820@naver.com>
2024-09-24 23:52:41 +0900

    🗑️remove: Timeline 컴포넌트 제거 (이전 Server 컴포넌트) - Client 컴포넌트로 리팩토링

--- src/app/_components/server/Timeline/components/TimelineItem/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as TimeStamp } from "./TimeStamp/TimeStamp";
-export { default as TimeItem } from "./TimeItem/TimeItem";
--- src/app/_components/server/Timeline/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as hexConvertForRGBA } from "./hexConvertForRGBA/hexConvertForRGBA";
