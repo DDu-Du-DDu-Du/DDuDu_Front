f05bffb2cacb3f69c322cb64ff103a7b0bdb8b72
seunghyun <tmdgus7820@naver.com>
2024-09-24 23:17:16 +0900

    💎feat: MainSchedule 페이지 작업 - 주 간 달력 UI 및 주차에 따른 일자 표시 - 주간 목표 설정 및 수정 - 투두에 시간 설정한 경우 타임 테이블에 표시되고 설정하지 않은 경우 투두는 별도로 표시되도록 작업

--- src/app/(route)/feed/components/MainSchedule/MainSchedule.tsx
+++ src/app/(route)/feed/components/MainSchedule/MainSchedule.tsx
@@ -1,5 +1,82 @@
-const MainSchedule = () => {
-  return <>스케줄</>;
+"use client";
+
+import { Fragment } from "react";
+
+import { Timeline } from "@/app/_components/client";
+import { GoalsType } from "@/app/_components/client/Calender/FeedCalender/FeedCalender";
+import WeekCalendar from "@/app/_components/client/Calender/WeekCalendar/WeekCalendar";
+import { FEED_KEY } from "@/app/_constants/queryKey/queryKey";
+import { getDailyTimeTable, getGoals, getWeeklyDDuDus } from "@/app/_services/client";
+import { MainDailyTimeTableType, MonthlyWeeklyDDuDuType } from "@/app/_types/response/feed/feed";
+import { useQuery } from "@tanstack/react-query";
+
+import MainGoalItem from "../MainGoalItem/MainGoalItem";
+
+import { useSession } from "next-auth/react";
+
+interface MainScheduleProps {
+  selectedDDuDuDate: string;
+}
+
+const MainSchedule = ({ selectedDDuDuDate }: MainScheduleProps) => {
+  // 임시 DAILY TIME TABLE
+
+  const { data: session } = useSession();
+
+  const { data: weeklyDDuDus } = useQuery<MonthlyWeeklyDDuDuType[]>({
+    queryKey: [FEED_KEY.WEEKLY_DDUDUS, selectedDDuDuDate],
+    queryFn: () =>
+      getWeeklyDDuDus({
+        accessToken: session?.sessionToken as string,
+        userId: session?.user.userId as number,
+        date: selectedDDuDuDate,
+      }),
+  });
+
+  const { data: dailyTimeTable } = useQuery<MainDailyTimeTableType>({
+    queryKey: [FEED_KEY.DAILY_TIMETABLE, selectedDDuDuDate],
+    queryFn: () =>
+      getDailyTimeTable({
+        accessToken: session?.sessionToken as string,
+        userId: session?.user.userId as number,
+        date: selectedDDuDuDate,
+      }),
+  });
+
+  const { data: weeklyGoals } = useQuery<GoalsType>({
+    queryKey: [FEED_KEY.WEEKLY_GOALS, selectedDDuDuDate],
+    queryFn: () =>
+      getGoals({
+        accessToken: session?.sessionToken as string,
+        type: "WEEK",
+        date: selectedDDuDuDate,
+      }),
+  });
+
+  return (
+    <div className="bg-example_gray_100">
+      <WeekCalendar
+        weeklyDDuDus={weeklyDDuDus || []}
+        weeklyGoals={weeklyGoals}
+        selectedDDuDuDate={selectedDDuDuDate}
+      />
+      <Timeline
+        timeline={dailyTimeTable?.timetable || []}
+        selectedDDuDuDate={selectedDDuDuDate}
+      />
+      <ul className="bg-white_100 py-[2.2rem] px-[2.4rem]">
+        {dailyTimeTable?.unassignedDdudus.map(({ goal, ddudus }, index) => (
+          <Fragment key={index}>
+            <MainGoalItem
+              goal={goal}
+              ddudus={ddudus}
+              selectedDDuDuDate={selectedDDuDuDate}
+            />
+          </Fragment>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MainSchedule;
