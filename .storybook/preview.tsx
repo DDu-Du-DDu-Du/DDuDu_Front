1ccdfb8f4e5b54c07e936352d2aa911acd87d939
seunghyun <tmdgus7820@naver.com>
2024-09-24 23:16:29 +0900

    💎feat: Storybook의 preview에 query, session Provider 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,10 @@
 import "../src/app/_styles/globals.css";
 import "./preview.css";
 
+import { TanstackProvider } from "@/app/(route)/_components";
 import type { Preview } from "@storybook/react";
+
+import { SessionProvider } from "next-auth/react";
 
 const preview: Preview = {
   parameters: {
@@ -17,7 +20,11 @@ const preview: Preview = {
     (Story, { args }) => {
       return (
         <main id="app">
-          <Story args={args} />
+          <SessionProvider>
+            <TanstackProvider>
+              <Story args={args} />
+            </TanstackProvider>
+          </SessionProvider>
         </main>
       );
     },
