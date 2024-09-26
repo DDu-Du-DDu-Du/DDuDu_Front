04d4b3db529240fcad61147661418f4510e41821
seunghyun <tmdgus7820@naver.com>
2024-09-26 18:25:30 +0900

    💎feat: padDateNumber 유틸 함수 분리 - 10미만의 숫자 앞자리에 문자열 0을 붙여 return 해주는 함수

--- src/app/_utils/index.ts
+++ src/app/_utils/index.ts
@@ -2,3 +2,4 @@ export { default as scrollToTop } from "./scrollToTop/scrollToTop";
 export { default as formatDateRange } from "./formatDateRange/formatDateRange";
 export { default as formatDateToYYYYMMDD } from "./formatDateToYYYYMMDD/formatDateToYYYYMMDD";
 export { default as getDayOfWeek } from "./getDayOfWeek/getDayOfWeek";
+export { default as padDateNumber } from "./padDateNumber/padDateNumber";
