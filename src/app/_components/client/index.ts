6b4ae4331a20b3b26ff523a1379b66dfc41110b7
seunghyun <tmdgus7820@naver.com>
2024-09-30 20:19:48 +0900

    🐛fix: rebase merge 시 잘못 병합한 Components index.ts 수정

--- src/app/_components/client/index.ts
+++ src/app/_components/client/index.ts
@@ -28,5 +28,3 @@ export { default as UserListItem } from "./UserListItem/UserListItem";
 export { default as Timeline } from "./Timeline/Timeline";
 
 export { default as SelectUiDiv } from "./SelectUiDiv/SelectUiDiv";
-
-export { default as SelectUiDiv } from "./SelectUiDiv/SelectUiDiv";
