0018b394777702b650b114ed8d36fcd6f92886eb
seunghyun <tmdgus7820@naver.com>
2024-09-30 20:29:33 +0900

    🚜change: root page 수정

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,22 +1,10 @@
-// import { auth } from "@/auth";
 import { signInWithKakao } from "../_api/serverActions/auth";
 
 import Link from "next/link";
 
-// import { QUERY_KEY, QUERY_OPTIONS, getQueryClient } from "../_constants";
-
 const Home = async () => {
-  // const queryClient = getQueryClient();
-
-  // await queryClient.prefetchQuery(QUERY_OPTIONS.TEST());
-
-  // const data = queryClient.getQueryData(QUERY_KEY.TEST);
-
-  // console.log(data);
-
   return (
     <main className="h-full w-full">
-      DDuDu
       <form action={signInWithKakao}>
         <button type="submit">로그인 버튼 (테스트용)</button>
       </form>
