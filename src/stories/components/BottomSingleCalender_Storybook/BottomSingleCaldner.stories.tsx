2cdaf7410d4db1c0970f4a2ecde50567a863bfaa
seunghyun <tmdgus7820@naver.com>
2024-10-01 03:27:44 +0900

    🐛fix: Story 파일 수정 - 전체적으로 변경된 경로 및 파일 수정 - (header) 파일 불필요해 제거

--- src/stories/components/BottomSingleCalender_Storybook/BottomSingleCaldner.stories.tsx
+++ src/stories/components/BottomSingleCalender_Storybook/BottomSingleCaldner.stories.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { BottomSingleCalender } from "@/app/_components/client/Calender";
+import { BottomSingleCalendar } from "@/app/_components/client/Calendar";
 import type { Meta, StoryObj } from "@storybook/react";
 
 /**
@@ -13,33 +13,35 @@ import type { Meta, StoryObj } from "@storybook/react";
  * 캘린더를 사용하고자 하는 영역에서 useState를 통해 state와 setter를 연결해주세요!
  * */
 const meta = {
-  title: "components/Calender/BottomSingleCalender",
-  component: BottomSingleCalender,
+  title: "components/Calendar/BottomSingleCalendar",
+  component: BottomSingleCalendar,
   parameters: {},
 
   tags: ["autodocs"],
 
   argTypes: {
-    selected: {
-      control: { disable: true },
+    selectedDate: {
+      control: { type: "date", disable: true },
       description: "캘린더에서 선택한 날짜 데이터들을 담고 있는 State입니다.",
     },
-
     setSelected: {
       control: { disable: true },
       description: "Bottom Sheet가 오픈될지에 대한 `boolean`값을 전달받습니다.",
     },
   },
   args: {},
-} satisfies Meta<typeof BottomSingleCalender>;
+} satisfies Meta<typeof BottomSingleCalendar>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    selected: new Date(),
+    selectedDate: new Date(),
+    currentDate: "2024-10-01",
     setSelected: () => {},
+    onChangeDDuDuDate: () => {},
+    handleCalendarSheetToggleOff: () => {},
   },
   decorators: [
     (BottomMultipleCalender) => {
@@ -51,7 +53,7 @@ export const Default: Story = {
 
       return (
         <section className="px-4">
-          <BottomMultipleCalender args={{ selected, setSelected: selectedHandler }} />
+          <BottomMultipleCalender args={{ selectedDate: selected, setSelected: selectedHandler }} />
           <span>
             <p>{selected?.toString()}</p>
           </span>
--- src/stories/components/MainFeedCalender_Storybook/MainFeedCalender.stories.tsx
+++ src/stories/components/MainFeedCalender_Storybook/MainFeedCalender.stories.tsx
@@ -1,9 +1,168 @@
-import { FeedCalender } from "@/app/_components/client/Calender";
-import { DAILY_DDUDU_MOCK_DATA } from "@/app/_components/client/Calender/FeedCalender/components/DailyDDuDu/DailyDDuDu.constant";
+import { FeedCalendar } from "@/app/_components/client/Calendar";
+import { GoalsType } from "@/app/_components/client/Calendar/FeedCalendar/FeedCalendar";
 import type { StoryObj } from "@storybook/react";
 
+const MONTHLY_GOALS_MOCK_DATA: GoalsType = {
+  id: 5,
+  contents: "9월 목표 \n- 뚜두 프로젝트 배포하기\n- MVP 완료하기",
+  type: "MONTH",
+};
+
+const DAILY_DDUDU_MOCK_DATA = [
+  {
+    date: "2024-09-01",
+    totalCount: 0,
+    uncompletedCount: 0,
+  },
+  {
+    date: "2024-09-02",
+    totalCount: 0,
+    uncompletedCount: 0,
+  },
+  {
+    date: "2024-09-03",
+    totalCount: 0,
+    uncompletedCount: 0,
+  },
+  {
+    date: "2024-09-04",
+    totalCount: 0,
+    uncompletedCount: 0,
+  },
+  {
+    date: "2024-09-05",
+    totalCount: 0,
+    uncompletedCount: 0,
+  },
+  {
+    date: "2024-09-06",
+    totalCount: 1,
+    uncompletedCount: 1,
+  },
+  {
+    date: "2024-09-07",
+    totalCount: 1,
+    uncompletedCount: 1,
+  },
+  {
+    date: "2024-09-08",
+    totalCount: 1,
+    uncompletedCount: 1,
+  },
+  {
+    date: "2024-09-09",
+    totalCount: 4,
+    uncompletedCount: 4,
+  },
+  {
+    date: "2024-09-10",
+    totalCount: 5,
+    uncompletedCount: 5,
+  },
+  {
+    date: "2024-09-11",
+    totalCount: 2,
+    uncompletedCount: 2,
+  },
+  {
+    date: "2024-09-12",
+    totalCount: 7,
+    uncompletedCount: 7,
+  },
+  {
+    date: "2024-09-13",
+    totalCount: 4,
+    uncompletedCount: 4,
+  },
+  {
+    date: "2024-09-14",
+    totalCount: 1,
+    uncompletedCount: 1,
+  },
+  {
+    date: "2024-09-15",
+    totalCount: 2,
+    uncompletedCount: 2,
+  },
+  {
+    date: "2024-09-16",
+    totalCount: 2,
+    uncompletedCount: 2,
+  },
+  {
+    date: "2024-09-17",
+    totalCount: 2,
+    uncompletedCount: 2,
+  },
+  {
+    date: "2024-09-18",
+    totalCount: 2,
+    uncompletedCount: 2,
+  },
+  {
+    date: "2024-09-19",
+    totalCount: 4,
+    uncompletedCount: 3,
+  },
+  {
+    date: "2024-09-20",
+    totalCount: 3,
+    uncompletedCount: 3,
+  },
+  {
+    date: "2024-09-21",
+    totalCount: 1,
+    uncompletedCount: 1,
+  },
+  {
+    date: "2024-09-22",
+    totalCount: 4,
+    uncompletedCount: 4,
+  },
+  {
+    date: "2024-09-23",
+    totalCount: 4,
+    uncompletedCount: 0,
+  },
+  {
+    date: "2024-09-24",
+    totalCount: 3,
+    uncompletedCount: 3,
+  },
+  {
+    date: "2024-09-25",
+    totalCount: 3,
+    uncompletedCount: 0,
+  },
+  {
+    date: "2024-09-26",
+    totalCount: 4,
+    uncompletedCount: 3,
+  },
+  {
+    date: "2024-09-27",
+    totalCount: 2,
+    uncompletedCount: 2,
+  },
+  {
+    date: "2024-09-28",
+    totalCount: 1,
+    uncompletedCount: 1,
+  },
+  {
+    date: "2024-09-29",
+    totalCount: 1,
+    uncompletedCount: 1,
+  },
+  {
+    date: "2024-09-30",
+    totalCount: 2,
+    uncompletedCount: 2,
+  },
+];
+
 /**
- * ## MainFeedCalender Component
+ * ## MainFeedCalendar Component
  *
  * ### Props
  * - **monthlyGoals : **문자열 배열 형태로 이루어진 월간 목표 데이터를 입력받습니다.
@@ -11,8 +170,8 @@ import type { StoryObj } from "@storybook/react";
  *
  * */
 const meta = {
-  title: "components/Calender/MainFeedCalender",
-  component: FeedCalender,
+  title: "components/Calendar/MainFeedCalendar",
+  component: FeedCalendar,
 
   tags: ["autodocs"],
 
@@ -34,13 +193,13 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: { monthlyGoals: ["아침 7시 기상", "기술면접 준비"], monthlyDDuDus: DAILY_DDUDU_MOCK_DATA },
+  args: { monthlyGoals: MONTHLY_GOALS_MOCK_DATA, monthlyDDuDus: DAILY_DDUDU_MOCK_DATA },
   decorators: [
     () => {
       return (
-        <FeedCalender
+        <FeedCalendar
           monthlyDDuDus={DAILY_DDUDU_MOCK_DATA}
-          monthlyGoals={["아침 7시 기상", "기술면접 준비"]}
+          monthlyGoals={MONTHLY_GOALS_MOCK_DATA}
         />
       );
     },
