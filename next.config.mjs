123e662514da1e1f88296fc9853f078b2e4f7ffb
seunghyun <tmdgus7820@naver.com>
2024-10-05 16:20:23 +0900

    🐛fix: 배포 후 mixed content 오류 발생 - 오류 해결을 위한 rewrites 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: "/api/auth/:path*",
+      },
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
