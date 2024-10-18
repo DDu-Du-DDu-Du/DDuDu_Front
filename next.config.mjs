377d1916dabe44b72d9b8be289f9cf0336d65818
seunghyun <tmdgus7820@naver.com>
2024-10-18 15:01:38 +0900

    📝docs: next.config rewrite 설정 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: "/api/auth/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
