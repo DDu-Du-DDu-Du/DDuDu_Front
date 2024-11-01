b4119460b12db3ea7bedd37d8482b91637817a1f
seunghyun <tmdgus7820@naver.com>
2024-11-01 21:36:00 +0900

    💎feat: middleware를 통한 router 및 session 관리 - 로그인했는데 /login 페이지 진입 시 BaseUrl로 접근하도록 코드 추가 - /login 경로가 아닌 경우 필수적으로 로그인을 해야 하기 때문에 session 정보가 없는 경우 로그인 페이지로 이동하도록 코드 추가

---
+++ src/app/_constants/auth/auth.ts
@@ -0,0 +1,7 @@
+export const AUTH_ROUTES = {
+  LOGIN: "/login",
+};
+
+export const BASE_URL = "/feed?view=ddudu";
+
+export const PUBLIC_ROUTES = [AUTH_ROUTES.LOGIN];
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,35 @@
+import { AUTH_ROUTES, BASE_URL, PUBLIC_ROUTES } from "./app/_constants/auth/auth";
 import { auth } from "./auth";
 
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  // 아래 Matcher 경로에서만 미들웨어가 실행됩니다.
-  // ! 개발 편의성을 위해 미들웨어 실행을 풀어놓았습니다.
-  // TODO 추후 로그인이 필요한 페이지를 정리한 후 matcher에 추가해주어야 합니다.
-  matcher: ["/none-middleware-call"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
 
 export async function middleware(request: NextRequest) {
-  try {
-    const session = await auth();
+  const session = await auth();
+  const { pathname } = request.nextUrl;
+  const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
 
-    if (!session || !session.user) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    return NextResponse.next();
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (isPublicRoute && session) {
+    return NextResponse.redirect(new URL(BASE_URL, request.url));
   }
+
+  if (!isPublicRoute && !session) {
+    console.log("??");
+    return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, request.url));
+  }
+
+  return NextResponse.next();
 }
