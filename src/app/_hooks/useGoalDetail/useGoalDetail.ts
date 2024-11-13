131a74f2d9898ce6f380bb657622dac468702c14
seunghyun <tmdgus7820@naver.com>
2024-11-13 21:24:38 +0900

    💎feat: useGoalDetail hook 생성 - GoalDetailData를 useQuery를 통해 가져오는 hook

---
+++ src/app/_hooks/useGoalDetail/useGoalDetail.ts
@@ -0,0 +1,25 @@
+import { GOAL_KEY } from "@/app/_constants/queryKey/queryKey";
+import { getGoalEditorData } from "@/app/_services/client/goalEditor";
+import { GoalDetailType } from "@/app/_types/response/goal/goal";
+import { useQuery } from "@tanstack/react-query";
+
+import { useSession } from "next-auth/react";
+
+interface UseGoalDetailProps {
+  goalId: string;
+}
+
+const useGoalDetail = ({ goalId }: UseGoalDetailProps) => {
+  const { data: session } = useSession();
+
+  const { data: goalDetail } = useQuery<GoalDetailType>({
+    queryKey: [GOAL_KEY.GOAL_EDITOR, goalId],
+    queryFn: () => getGoalEditorData(session?.sessionToken as string, goalId),
+  });
+
+  return {
+    goalDetail,
+  };
+};
+
+export default useGoalDetail;
