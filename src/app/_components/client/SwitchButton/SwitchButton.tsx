59284d6f4bdc9bb069e6c42e511b7977fbceb3ef
SeungHyune <tmdgus7820@naver.com>
2025-01-05 17:51:39 +0900

    🎨style: MainFeed의 SwitchButton 색상 추가

--- src/app/_components/client/SwitchButton/SwitchButton.tsx
+++ src/app/_components/client/SwitchButton/SwitchButton.tsx
@@ -26,22 +26,28 @@ const SwitchButton = ({
   });
 
   return (
-    <div className="relative flex flex-1 w-[16rem] h-[3rem] bg-example_gray_300 text-size13 rounded-[5rem] cursor-pointer select-none">
+    <div className="relative flex flex-1 w-[16rem] h-[3rem] bg-white_100 text-size13 rounded-[5rem] cursor-pointer select-none shadow-md">
       <div
-        className="relative flex justify-center items-center w-[50%] box-border z-10"
+        className={twJoin(
+          "relative flex justify-center items-center w-[50%] box-border z-10",
+          toggle === selectedOption ? "text-white_100 font-medium" : "",
+        )}
         onClick={handleToggleToFirst}
       >
         {firstLabel}
       </div>
       <div
-        className="relative flex justify-center items-center w-[50%] box-border z-10"
+        className={twJoin(
+          "relative flex justify-center items-center w-[50%] box-border z-10",
+          toggle === selectedOption ? "" : "text-white_100 font-medium",
+        )}
         onClick={handleToggleToSecond}
       >
         {secondLabel}
       </div>
       <div
         className={twJoin(
-          "absolute w-[50%] h-[3rem] bg-white_100 border-solid border-2 border-example_gray_500 rounded-[5rem] transition-all duration-200 ease-in-out",
+          "absolute w-[50%] h-[3rem] bg-main border-solid border-2 border-example_gray_100 rounded-[5rem] transition-all duration-200 ease-in-out",
           toggle === selectedOption ? "translate-x-0" : "translate-x-[100%]",
         )}
       />
