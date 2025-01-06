e6501c967a67019cd44b823e3f1127d366bbdb83
seunghyun <tmdgus7820@naver.com>
2025-01-06 23:04:24 +0900

    🎨style: CheckboxInput 컴포넌트 스타일 수정 - checked & checkbox hover 배경색 수정

--- src/app/_components/client/CheckboxInput/CheckboxInput.tsx
+++ src/app/_components/client/CheckboxInput/CheckboxInput.tsx
@@ -64,12 +64,12 @@ const CheckboxInput = ({
         htmlFor={id || inputId}
         className={twMerge(
           "block border-solid border-[1px] border-example_gray_100 rounded-radius10 select-none",
-          isChecked && "bg-example_gray_300 font-medium",
+          isChecked && "bg-example_gray_100 font-medium",
           type === "word" &&
             "p-[1.2rem] min-w-[4rem] flex items-center justify-center text-[1.3rem]",
           disabled
             ? "opacity-40 cursor-default"
-            : "hover:bg-example_gray_300 hover:font-medium cursor-pointer",
+            : "hover:bg-example_gray_100 hover:font-medium cursor-pointer",
           className,
         )}
         style={{
