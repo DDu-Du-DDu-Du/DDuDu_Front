396cd6a68c155ee729a4a7b81df59d8a146e3a04
seunghyun <tmdgus7820@naver.com>
2025-01-16 15:04:45 +0900

    🐛fix: metadata의 viewport 수정 - next.js 공식 문서 참고하여 수정

--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -7,17 +7,26 @@ import { BottomProvider } from "../_components/client/BottomSheet/components";
 import { TanstackProvider } from "./_components";
 import SessionChecker from "./_components/SessionChecker/SessionChecker";
 
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+
+export const viewport: Viewport = {
+  themeColor: "#1363de",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
+};
 
 export const metadata: Metadata = {
   title: "뚜두뚜두",
   description: "목표를 설정하고 성취하며 계획을 관리하는 투두 서비스",
-  icons: {
-    icon: "/icons/favicon.ico",
-  },
   manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 interface RootLayoutProps {
@@ -31,10 +40,6 @@ const RootLayout = ({ children, session }: Readonly<RootLayoutProps>) => {
       lang="ko"
       className={SpoqaHanSansFont.className}
     >
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover"
-      />
       <body>
         <SessionProvider session={session}>
           <SessionChecker>
